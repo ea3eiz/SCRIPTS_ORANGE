e4c945686705e522432b35eeb396d4552e02b7f1
ea3eiz <ea3eiz@ea3eiz.com>
2017-04-04 20:50:49 +0200

    1

--- ejecutar_d-star29.sh
+++ ejecutar_d-star29.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-cd /home/pi/SCRIPTS
+cd /home/orangepi/SCRIPTS
 
-status=`grep "MMDVMBM" /home/pi/status.ini`
+status=`grep "MMDVMBM" /home/orangepi/status.ini`
                         status1=`expr substr $status 9 2`
 
-status2=`grep "MMDVMPLUS" /home/pi/status.ini`
+status2=`grep "MMDVMPLUS" /home/orangepi/status.ini`
                         status3=`expr substr $status2 11 2` 
 
                         if [ $status1 = "ON" ]
                         then
                         sleep 1
 #======Pone D-STAR_ON y quietA D-STAR_OFF===
-cd /home/pi/Desktop/
-sudo mv D-STAR_OFF /home/pi/
+cd /home/orangepi/Desktop/
+sudo mv D-STAR-OFF.desktop /home/orangepi/botones_off
 
-cd /home/pi/
-sudo mv D-STAR_ON /home/pi/Desktop/
+cd /home/orangepi/botones_on
+sudo mv D-STAR-ON.desktop /home/orangepi/Desktop/
 #==================================
 sudo ircddbgateway -gui
 
@@ -25,36 +25,36 @@ else
                         then
                         sleep 1
  #======Pone D-STAR_ON y quietA D-STAR_OFF===
-cd /home/pi/Desktop/
-sudo mv D-STAR_OFF /home/pi/
+cd /home/orangepi/Desktop/
+sudo mv D-STAR-OFF.desktop /home/orangepi/botones_off
 
-cd /home/pi/
-sudo mv D-STAR_ON /home/pi/Desktop/
+cd /home/orangepi/botones_on
+sudo mv D-STAR-ON.desktop /home/orangepi/Desktop/
 #==================================                       
 sudo ircddbgateway -gui  
 else
 
 
-status2=`grep "dstarrepeater" /home/pi/status.ini`
+status2=`grep "dstarrepeater" /home/orangepi/status.ini`
                         status3=`expr substr $status2 15 2`
                         if [ $status3 = "ON" ]
                         then
                         sleep 1
  #======Pone D-STAR_ON y quietA D-STAR_OFF===
-cd /home/pi/Desktop/
-sudo mv D-STAR_OFF /home/pi/
+cd /home/orangepi/Desktop/
+sudo mv D-STAR-OFF.desktop /home/orangepi/botones_off
 
-cd /home/pi/
-sudo mv D-STAR_ON /home/pi/Desktop/
-#==================================                       
+cd /home/orangepi/botones_on
+sudo mv D-STAR-ON.desktop /home/orangepi/Desktop/
+#==================================                      
 sudo ircddbgateway -gui 
 
 else
 
-cd /home/pi/SCRIPTS
+cd /home/orangepi/SCRIPTS
 
 lxterminal --geometry=80x24 -e sudo sh menu_ejecutar_d-star29.sh
-#cd /home/pi/SCRIPTS
+#cd /home/orangepi/SCRIPTS
 #sudo sh menu_ejecutar_d-star.sh
 fi
 fi
