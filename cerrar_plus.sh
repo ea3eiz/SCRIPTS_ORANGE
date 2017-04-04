cda5d77b32aba6e15d18b4bc291556b3e7157117
ea3eiz <ea3eiz@ea3eiz.com>
2017-04-04 20:28:44 +0200

    1

--- cerrar_plus.sh
+++ cerrar_plus.sh
@@ -2,7 +2,7 @@
 cd /home/orangepi/Desktop/
 sudo mv DMR+_ON.desktop /home/orangepi/botones_on
 
-cd /home/pi/botones_off
+cd /home/orangepi/botones_off
 sudo mv DMR+_OFF.desktop /home/orangepi/Desktop/
 
 sudo killall MMDVMPLUS
