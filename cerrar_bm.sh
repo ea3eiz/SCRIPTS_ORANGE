c83cd682bfda41d1a2d942ec3db3eb2c9d112616
ea3eiz <ea3eiz@ea3eiz.com>
2017-04-04 12:34:16 +0200

    1

--- cerrar_bm.sh
+++ cerrar_bm.sh
@@ -2,7 +2,7 @@
 cd /home/orangepi/Desktop/
 sudo mv BM-ON.desktop /home/pi/botones_on
 
-cd /homeorange/pi/botones_off
+cd /home/orange/pi/botones_off
 sudo mv BM-OFF.desktop /home/orangepi/Desktop/
 
 sudo killall MMDVMBM
