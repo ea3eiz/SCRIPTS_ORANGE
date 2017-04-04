98537ef782e9bdb9aead46f2105e61745c394b37
ea3eiz <ea3eiz@ea3eiz.com>
2017-04-04 20:24:55 +0200

    1

--- cerrar_plus.sh
+++ cerrar_plus.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /home/pi/Desktop/
-sudo mv PLUS_ON /home/pi/
+cd /home/orangepi/Desktop/
+sudo mv DMR+_ON.desktop /home/orangepi/botones_on
 
-cd /home/pi/
-sudo mv PLUS_OFF /home/pi/Desktop/
+cd /home/pi/botones_off
+sudo mv DMR+_OFF.desktop /home/orangepi/Desktop/
 
 sudo killall MMDVMPLUS
-sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
+sed -i "6c MMDVMPLUS=OFF" /home/orangepi/status.ini
