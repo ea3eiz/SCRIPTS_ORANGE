9753fac10b4ad3716b20224825bee773ba541f70
ea3eiz <ea3eiz@ea3eiz.com>
2017-09-14 14:05:55 +0200

    1

--- cerrar_bm_05.sh
+++ cerrar_bm_05.sh
@@ -7,6 +7,6 @@ cd ~/SCRIPTS_ORANGE
 sed -i "7c MMDVMBM=OFF" ~/status.ini
 
 
-sed -i "6cExec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/Abrir_MMDVMBM
-sed -i "7c Icon=~/SCRIPTS_ORANGE/DMR.png" ~/Desktop/Abrir_MMDVMBM
-sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/Abrir_MMDVMBM
+sed -i "6cExec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/BM.desktop
+sed -i "7c Icon=~/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
+sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/BM.desktop
