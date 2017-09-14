5b5a91f50a6b6ed2d17db63c07973fc3c8b8fe32
ea3eiz <ea3eiz@ea3eiz.com>
2017-09-14 13:32:21 +0200

    1

--- ejecutar_bm_05.sh
+++ ejecutar_bm_05.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 sleep 10
- #status=`grep "BlueDV" /home/pi/status.ini`
+ #status=`grep "BlueDV" ~/status.ini`
                         #status1=`expr substr $status 8 2`
                         #if [ $status1 = "ON" ]
                         #then
                  
 #===================
-#cd /home/pi/V30
+#cd ~/V30
 #lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
 #===================
 #else
-#status2=`grep "dstarrepeater" /home/pi/status.ini`
+#status2=`grep "dstarrepeater" ~/status.ini`
                         #status3=`expr substr $status2 15 2`
                         #if [ $status3 = "ON" ]
                         #then
                      
 #===================
-#cd /home/pi/V30
+#cd ~/V30
 #lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
 #===================
 
 #else
 
 
-sed -i "4c Exec=sh -c 'cd /home/pi/V30; lxterminal --geometry=72x15 -e sudo sh cerrar_bm_30.sh'" /home/pi/Desktop/Abrir_MMDVMBM
-sed -i "5c Icon=/home/pi/V30/BRANDMEISTER_ON.png" /home/pi/Desktop/Abrir_MMDVMBM
-sed -i "10c Name[es_ES]=Cerrar BM" /home/pi/Desktop/Abrir_MMDVMBM
+sed -i "4c Exec=sh -c 'cd ~/V30; lxterminal --geometry=72x15 -e sudo sh cerrar_bm_30.sh'" ~/Desktop/Abrir_MMDVMBM
+sed -i "5c Icon=~/V30/BRANDMEISTER_ON.png" ~/Desktop/Abrir_MMDVMBM
+sed -i "10c Name[es_ES]=Cerrar BM" ~/Desktop/Abrir_MMDVMBM
 
-sed -i "7c MMDVMBM=ON" /home/pi/status.ini
+sed -i "7c MMDVMBM=ON" ~/status.ini
 
 
-cd /home/pi/MMDVMHost
+cd ~/MMDVMHost
 echo "\33[1;32m"
 clear
 echo "***********************************************"
