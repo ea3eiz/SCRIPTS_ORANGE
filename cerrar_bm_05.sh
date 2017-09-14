a8ee4a5ed3a89f319b7a0dd1d11b76c8d4f91542
ea3eiz <ea3eiz@ea3eiz.com>
2017-09-14 13:45:45 +0200

    1

--- cerrar_bm_05.sh
+++ cerrar_bm_05.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sudo killall MMDVMBM
-cd /home/pi/V30
+cd ~/SCRIPTS_ORANGE
 #sudo sh cerrar_ysf_30.sh
 #sudo sh cerrar_d-star_30.sh
-sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
+sed -i "7c MMDVMBM=OFF" ~/status.ini
 
 
-sed -i "4cExec=sh -c 'cd /home/pi/V30; lxterminal --geometry=72x15 -e sudo sh ejecutar_bm_30.sh'" /home/pi/Desktop/Abrir_MMDVMBM
-sed -i "5c Icon=/home/pi/V30/DMR.png" /home/pi/Desktop/Abrir_MMDVMBM
-sed -i "10c Name[es_ES]=Abrir BM" /home/pi/Desktop/Abrir_MMDVMBM
+sed -i "6cExec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/Abrir_MMDVMBM
+sed -i "7c Icon=~/SCRIPTS_ORANGE/DMR.png" ~/Desktop/Abrir_MMDVMBM
+sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/Abrir_MMDVMBM
