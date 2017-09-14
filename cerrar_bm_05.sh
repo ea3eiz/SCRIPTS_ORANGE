625b6e4d332d1904481ea52a161df959aa4a058e
ea3eiz <ea3eiz@ea3eiz.com>
2017-09-14 14:15:34 +0200

    1

--- cerrar_bm_05.sh
+++ cerrar_bm_05.sh
@@ -8,5 +8,5 @@ sed -i "7c MMDVMBM=OFF" ~/status.ini
 
 
 sed -i "6cExec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/BM.desktop
-sed -i "7c Icon=~/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/BM.desktop
--- ejecutar_bm_05.sh
+++ ejecutar_bm_05.sh
@@ -24,7 +24,7 @@
 
 
 sed -i "6cExec=mate-terminal -x sh cerrar_bm_05.sh" ~/Desktop/BM.desktop
-sed -i "7c Icon=~/SCRIPTS_ORANGE/BRANDMEISTER_ON.png" ~/Desktop/BM.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BRANDMEISTER_ON.png" ~/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Cerrar BM" ~/Desktop/BM.desktop
 
 
