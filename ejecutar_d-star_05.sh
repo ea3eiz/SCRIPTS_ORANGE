36cb43d69e1a97978f1ebb7ed6b887f38de815e4
ea3eiz <ea3eiz@ea3eiz.com>
2017-09-25 12:28:44 +0200

    1

--- ejecutar_d-star_05.sh
+++ ejecutar_d-star_05.sh
@@ -5,7 +5,7 @@
 #sed -i "4c Exec=sh -c 'cd ~/V30; sudo sh cerrar_d-star_30.sh'" ~/Desktop/ircDDBGateway
 #sed -i "5c Icon=~/V30/DSTAR_ON.png" ~/Desktop/ircDDBGateway
 #sed -i "10c Name[es_ES]=Cerrar ircDDB" ~/Desktop/ircDDBGateway
-sudo ircddbgateway -gui
+ircddbgateway -gui
 #sleep 3
 #sudo ircddbgateway -gui
 
