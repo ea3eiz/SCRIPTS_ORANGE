5b8ba648dfb7b2d7b13272bf468042851dd5233d
ea3eiz <ea3eiz@ea3eiz.com>
2017-09-25 12:19:52 +0200

    1

--- ejecutar_d-star_05.sh
+++ ejecutar_d-star_05.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-status6=`grep "BlueDV" ~/status.ini`
-                        status7=`expr substr $status6 8 2`
-#====================================   
-                     
-                        if [ $status7 = "ON" ]
-                        then
-
-cd ~/V30
-#sudo sh cerrar_ysf_30.sh
-sudo sh cerrar_bm_30.sh
-sudo sh cerrar_plus_30.sh
-sudo sh cerrar_mmdvmLIBRE_30.sh
-lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
-
-else
-
-#==================================
-sleep 5
-sed -i "1c D-STAR=ON" ~/status.ini
-sed -i "4c Exec=sh -c 'cd ~/V30; sudo sh cerrar_d-star_30.sh'" ~/Desktop/ircDDBGateway
-sed -i "5c Icon=~/V30/DSTAR_ON.png" ~/Desktop/ircDDBGateway
-sed -i "10c Name[es_ES]=Cerrar ircDDB" ~/Desktop/ircDDBGateway
+#sleep 5
+#sed -i "1c D-STAR=ON" ~/status.ini
+#sed -i "4c Exec=sh -c 'cd ~/V30; sudo sh cerrar_d-star_30.sh'" ~/Desktop/ircDDBGateway
+#sed -i "5c Icon=~/V30/DSTAR_ON.png" ~/Desktop/ircDDBGateway
+#sed -i "10c Name[es_ES]=Cerrar ircDDB" ~/Desktop/ircDDBGateway
 sudo ircddbgateway -gui
-sleep 3
-sudo ircddbgateway -gui
-fi
+#sleep 3
+#sudo ircddbgateway -gui
+
