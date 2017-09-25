623331733ff0089f304599f4fd74b9bba685a72a
ea3eiz <ea3eiz@ea3eiz.com>
2017-09-25 11:55:12 +0200

    1

--- ejecutar_d-star_05.sh
+++ ejecutar_d-star_05.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-status6=`grep "BlueDV" /home/pi/status.ini`
+status6=`grep "BlueDV" ~/status.ini`
                         status7=`expr substr $status6 8 2`
 #====================================   
                      
                         if [ $status7 = "ON" ]
                         then
 
-cd /home/pi/V30
+cd ~/V30
 #sudo sh cerrar_ysf_30.sh
 sudo sh cerrar_bm_30.sh
 sudo sh cerrar_plus_30.sh
@@ -18,10 +18,10 @@ else
 
 #==================================
 sleep 5
-sed -i "1c D-STAR=ON" /home/pi/status.ini
-sed -i "4c Exec=sh -c 'cd /home/pi/V30; sudo sh cerrar_d-star_30.sh'" /home/pi/Desktop/ircDDBGateway
-sed -i "5c Icon=/home/pi/V30/DSTAR_ON.png" /home/pi/Desktop/ircDDBGateway
-sed -i "10c Name[es_ES]=Cerrar ircDDB" /home/pi/Desktop/ircDDBGateway
+sed -i "1c D-STAR=ON" ~/status.ini
+sed -i "4c Exec=sh -c 'cd ~/V30; sudo sh cerrar_d-star_30.sh'" ~/Desktop/ircDDBGateway
+sed -i "5c Icon=~/V30/DSTAR_ON.png" ~/Desktop/ircDDBGateway
+sed -i "10c Name[es_ES]=Cerrar ircDDB" ~/Desktop/ircDDBGateway
 sudo ircddbgateway -gui
 sleep 3
 sudo ircddbgateway -gui
