7e550987349ebd4553e8a7b788c9c156828875bc
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-02 00:06:14 +0200

    1

--- ejecutar_plus_05.sh
+++ ejecutar_plus_05.sh
@@ -1,43 +1,16 @@
 #!/bin/bash
-sleep 10
- #status=`grep "BlueDV" /home/pi/status.ini`
-                        #status1=`expr substr $status 8 2`
-                        #if [ $status1 = "ON" ]
-                        #then
-                        #sleep 1
-
-#===================
-#cd /home/pi/V30
-#lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
-#===================
-
-#else
-
-#status2=`grep "dstarrepeater" /home/pi/status.ini`
-                        #status3=`expr substr $status2 15 2`
-                        #if [ $status3 = "ON" ]
-                        #then
-                        #sleep 1
 
 
-#===================
-#cd /home/pi/V30
-#lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
-#===================
 
-#else
-#sudo sh cerrar_mmdvmLIBRE_30.sh
-#sudo sh cerrar_bm_30.sh
+#sed -i "4c Exec=sh -c 'cd /home/pi/V30;lxterminal --geometry=72x15 -e sudo sh cerrar_plus_30.sh'" ~/Desktop/Abrir_MMDVMPLUS
+#sed -i "5c Icon=/home/pi/V30/DMRPLUS_ON.png" ~/Desktop/Abrir_MMDVMPLUS
+#sed -i "10c Name[es_ES]=Cerrar DMR+" ~/Desktop/Abrir_MMDVMPLUS
 
-sed -i "4c Exec=sh -c 'cd /home/pi/V30;lxterminal --geometry=72x15 -e sudo sh cerrar_plus_30.sh'" /home/pi/Desktop/Abrir_MMDVMPLUS
-sed -i "5c Icon=/home/pi/V30/DMRPLUS_ON.png" /home/pi/Desktop/Abrir_MMDVMPLUS
-sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Desktop/Abrir_MMDVMPLUS
-
-sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
+#sed -i "6c MMDVMPLUS=ON" ~/status.ini
 
 
 echo "\33[1;37m"
-cd /home/pi/MMDVMHost
+cd ~/MMDVMHost
 echo "\33[1;32m"
 clear
 echo "***********************************************"
