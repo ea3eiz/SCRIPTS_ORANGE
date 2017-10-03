d68f96974f4cb7aad4bf16110ca6b0eb3df7c38b
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 23:18:31 +0200

    1

--- cerrar_bluedv_05.sh
+++ cerrar_bluedv_05.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sed -i "4cExec=sh -c 'cd /home/pi/V30; sudo sh ejecutar_bluedv_05.sh'" /home/pi/Desktop/Abrir_BlueDV
-sed -i "5c Icon=/home/pi/V30/BLUEDV.png" /home/pi/Desktop/Abrir_BlueDV
-sed -i "10c Name[es_ES]=Abrir BlueDV" /home/pi/Desktop/Abrir_BlueDV
+sed -i "6c Exec=sh ejecutar_bluedv_05.sh" ~/Desktop/BlueDV.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BLUEDV_ON.png" ~/Desktop/BlueDV.desktop
+sed -i "8c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/BlueDV.desktop
+sed -i "11c Name[es_ES]=Cerrar BlueDV" ~/Desktop/BlueDV.desktop
 
-sed -i "2c BlueDV=OFF" /home/pi/status.ini
+sed -i "2c BlueDV=OFF" ~/status.ini
 
 sudo su | ps aux | grep BlueDV.exe | grep -v grep | awk '{print $2}' | xargs kill
 
