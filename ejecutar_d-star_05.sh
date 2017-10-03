b6828b876b2350f1e87c7b65a08dbd1c94d331c9
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 22:45:14 +0200

    1

--- ejecutar_d-star_05.sh
+++ ejecutar_d-star_05.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "6c Exec=/home/orangepi/SCRIPTS_ORANGE/cerrar_d-star_05.sh" ~/Desktop/D-Star.desktop
+sed -i "6c Exec=/home/orangepi/SCRIPTS_ORANGE/sh cerrar_d-star_05.sh" ~/Desktop/D-Star.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DSTAR_ON.png" ~/Desktop/D-Star.desktop
 ircddbgateway -gui
