37ad12fa99671d46e46d3e1456c6a7dbd6ce24b7
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 13:38:16 +0200

    1

--- cerrar_plus_05.sh
+++ cerrar_plus_05.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo killall MMDVMPLUS
-sed -i "6c Exec=mate-terminal -x sh ejecutar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
+sed -i "6c Exec=mate-terminal --geometry=80x20 -x sh ejecutar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/ICONO_DMRPLUS.png" ~/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Abrir DMR+" ~/Desktop/DMRPLUS.desktop
--- ejecutar_plus_05.sh
+++ ejecutar_plus_05.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sed -i "6c Exec=mate-terminal -x sh cerrar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
+sed -i "6c Exec=mate-terminal --geometry=80x20 -x sh cerrar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMRPLUS_ON.png" ~/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Cerrar DMR+" ~/Desktop/DMRPLUS.desktop
-
 
 echo "\33[1;37m"
 cd ~/MMDVMHost
