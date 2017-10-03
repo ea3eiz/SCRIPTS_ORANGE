cdd38e05487fcf31f855c8012fb8c6099bc4cf7b
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-04 00:07:14 +0200

    1

--- ejecutar_bluedv_05.sh
+++ ejecutar_bluedv_05.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+sed -i "6c Exec=sh cerrar_bluedv_05.sh" ~/Desktop/BlueDV.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BLUEDV_ON.png" ~/Desktop/BlueDV.desktop
+sed -i "8c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/BlueDV.desktop
+sed -i "11c Name[es_ES]=Cerrar BlueDV" ~/Desktop/BlueDV.desktop
 #cd ~/SCRIPTS_ORANGE
 #sudo sh cerrar_bm_05.sh
 #sudo sh cerrar_plus_05.sh
@@ -11,9 +14,4 @@
 
 
 cd ~/bluedv
-sed -i "6c Exec=sh cerrar_bluedv_05.sh" ~/Desktop/BlueDV.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BLUEDV_ON.png" ~/Desktop/BlueDV.desktop
-sed -i "8c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/BlueDV.desktop
-sed -i "11c Name[es_ES]=Cerrar BlueDV" ~/Desktop/BlueDV.desktop
-sleep 05
 mono BlueDV.exe
