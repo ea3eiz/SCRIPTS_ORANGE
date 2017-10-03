f2702422867760a9ec38e593b3d06bd359329e9e
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 18:43:32 +0200

    1

--- cerrar_dv4_05.sh
+++ cerrar_dv4_05.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "6c Exec=mate-terminal -x sh ejecutar_dv4_05.sh" ~/Desktop/DV4mini.desktop
+sed -i "6c Exec=sh ejecutar_dv4_05.sh" ~/Desktop/DV4mini.desktop
 sed -i "7c Icon=/home/orangepi/dv4mini/dv4k.png" ~/Desktop/DV4mini.desktop
 sed -i "11c Name[es_ES]=Abrir DV4mini" ~/Desktop/DV4mini.desktop
 
--- ejecutar_dv4_05.sh
+++ ejecutar_dv4_05.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "6c Exec=mate-terminal -x sh cerrar_dv4_05.sh" ~/Desktop/DV4mini.desktop
+sed -i "6c Exec=sh cerrar_dv4_05.sh" ~/Desktop/DV4mini.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DV4_ON.png" ~/Desktop/DV4mini.desktop
 sed -i "11c Name[es_ES]=Cerrar DV4mini" ~/Desktop/DV4mini.desktop
 
