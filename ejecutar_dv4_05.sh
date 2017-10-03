1bf168de11be132dc053cdbc3173fe2fae0af953
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 17:50:13 +0200

    1

--- ejecutar_dv4_05.sh
+++ ejecutar_dv4_05.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sleep 5
-sed -i "4c DV4mini=ON" /home/pi/status.ini
-sed -i "4c Exec=sh -c 'cd /home/pi/V30; sudo sh cerrar_dv4_30.sh'" /home/pi/Desktop/Abrir_dv4mini
-sed -i "5c Icon=/home/pi/V30/DV4_ON.png" /home/pi/Desktop/Abrir_dv4mini
-sed -i "10c Name[es_ES]=Cerrar DV4mini" /home/pi/Desktop/Abrir_dv4mini
 
-cd /home/pi/dv4mini
+#sed -i "4c DV4mini=ON" /home/pi/status.ini
+#sed -i "4c Exec=sh -c 'cd /home/pi/V30; sudo sh cerrar_dv4_30.sh'" /home/pi/Desktop/Abrir_dv4mini
+#sed -i "5c Icon=/home/pi/V30/DV4_ON.png" /home/pi/Desktop/Abrir_dv4mini
+#sed -i "10c Name[es_ES]=Cerrar DV4mini" /home/pi/Desktop/Abrir_dv4mini
+
+cd ~/dv4mini
 mono dv4mini.exe
 
