6b03c76d0bb400f373b7c6ab8d3f3416d1bd08ad
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 22:55:31 +0200

    1

--- cerrar_d-star_05.sh
+++ cerrar_d-star_05.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-
 sed -i "6c Exec=sh ejecutar_d-star_05.sh" ~/Desktop/D-Star.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DSTAR.png" ~/Desktop/D-Star.desktop
+sed -i "10c Name[es_ES]=Abrir ircDDB" ~/Desktop/D-Star.desktop
 sed -i "11c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/D-Star.desktop
 
 #cierra ircDDBGateway
--- ejecutar_d-star_05.sh
+++ ejecutar_d-star_05.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sed -i "6c Exec=sh cerrar_d-star_05.sh" ~/Desktop/D-Star.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DSTAR_ON.png" ~/Desktop/D-Star.desktop
+sed -i "10c Name[es_ES]=Cerrar ircDDB" ~/Desktop/D-Star.desktop
 sed -i "11c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/D-Star.desktop
+
+#Abre ircDDBGateway
 ircddbgateway -gui
