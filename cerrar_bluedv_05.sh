8ce5c7e62fc2576c6fd69f8e2a202d93918c1c1e
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 23:20:53 +0200

    1

--- cerrar_bluedv_05.sh
+++ cerrar_bluedv_05.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sed -i "6c Exec=sh ejecutar_bluedv_05.sh" ~/Desktop/BlueDV.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BLUEDV_ON.png" ~/Desktop/BlueDV.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BLUEDV.png" ~/Desktop/BlueDV.desktop
 sed -i "8c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/BlueDV.desktop
-sed -i "11c Name[es_ES]=Cerrar BlueDV" ~/Desktop/BlueDV.desktop
+sed -i "11c Name[es_ES]=Abrir BlueDV" ~/Desktop/BlueDV.desktop
 
 sed -i "2c BlueDV=OFF" ~/status.ini
 
