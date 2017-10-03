eac2166256a50d761dae6c11b3442ed5a32df9f4
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 23:09:59 +0200

    1

--- ejecutar_bluedv_05.sh
+++ ejecutar_bluedv_05.sh
@@ -9,10 +9,9 @@ sudo sh cerrar_ysf_05.sh
 sudo sh cerrar_dstarrepeater_05.sh
 sudo sh cerrar_solodstar_05.sh
 
-#sed -i "2c BlueDV=ON" ~/status.ini
-#sed -i "4c Exec=sh -c 'cd ~/V30; sudo sh cerrar_bluedv_05.sh'" ~/Desktop/Abrir_BlueDV
-#sed -i "5c Icon=~/V30/BLUEDV_ON.png" ~/Desktop/Abrir_BlueDV
-#sed -i "10c Name[es_ES]=Cerrar BlueDV" ~/Desktop/Abrir_BlueDV
+sed -i "6c Exec=sh cerrar_bluedv_05.sh" ~/Desktop/BlueDV.Desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BLUEDV_ON.png" ~/Desktop/BlueDV.Desktop
+sed -i "8c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/BlueDV.Desktop
 
 cd ~/bluedv
 mono BlueDV.exe
