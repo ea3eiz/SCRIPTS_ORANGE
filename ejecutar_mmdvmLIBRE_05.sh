0a3101f005dc12caa4270ce357cc67efdb26bf7b
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 11:18:13 +0200

    1

--- ejecutar_mmdvmLIBRE_05.sh
+++ ejecutar_mmdvmLIBRE_05.sh
@@ -1,36 +1,10 @@
 #!/bin/bash
-sleep 10
- #status=`grep "BlueDV" /home/pi/status.ini`
-                        #status1=`expr substr $status 8 2`
-                        #if [ $status1 = "ON" ]
-                        #then
-                        #sleep 1
-#===================
-#cd /home/pi/V30
-#lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
-#===================
-#else
-#status2=`grep "dstarrepeater" /home/pi/status.ini`
-                        #status3=`expr substr $status2 15 2`
-                        #if [ $status3 = "ON" ]
-                        #then
-                        #sleep 1
-#===================
-#cd /home/pi/V30
-#lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
-#===================
 
-#else
+sed -i "6c Exec=mate-terminal -x sh cerrar_mmdvLIBRE_05.sh" ~/Desktop/Libre.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/LIBRE_ON.png" ~/Desktop/Libre.desktop
+sed -i "10c Name[es_ES]=Cerrar LIBRE" ~/Desktop/Libre.desktop
 
-
-sed -i "4c Exec=sh -c 'cd /home/pi/V30;lxterminal --geometry=72x15 -e sudo sh cerrar_mmdvmLIBRE_30.sh'" /home/pi/Desktop/Abrir_LIBRE
-sed -i "5c Icon=/home/pi/V30/LIBRE_ON.png" /home/pi/Desktop/Abrir_LIBRE
-sed -i "10c Name[es_ES]=Cerrar LIBRE" /home/pi/Desktop/Abrir_LIBRE
-
-sed -i "10c MMDVMLIBRE=ON" /home/pi/status.ini
-
-
-cd /home/pi/MMDVMHost
+cd ~/MMDVMHost
 echo "\33[1;32m"
 clear
 echo "***********************************************"
