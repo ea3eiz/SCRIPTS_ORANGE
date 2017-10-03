46d6d73750ebc206b340c34c97a6a205cc53b7af
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 13:39:21 +0200

    1

--- ejecutar_plus_05.sh
+++ ejecutar_plus_05.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "6c Exec=mate-terminal --geometry=80x20 -x sh cerrar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
+sed -i "6c Exec=mate-terminal --geometry=80x10 -x sh cerrar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMRPLUS_ON.png" ~/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Cerrar DMR+" ~/Desktop/DMRPLUS.desktop
 
