a05bf592eadd90b76ff05c1860c2d58bc5ec28f5
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 18:41:29 +0200

    1

--- cerrar_dv4_05.sh
+++ cerrar_dv4_05.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sed -i "6c Exec=mate-terminal -x sh ejecutar_dv4_05.sh" ~/Desktop/DV4mini.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DV4_ON.png" ~/Desktop/DV4mini.desktop
-sed -i "11c Name[es_ES]=Cerrar DV4mini" ~/Desktop/DV4mini.desktop
+sed -i "7c Icon=/home/orangepi/dv4mini/dv4k.png" ~/Desktop/DV4mini.desktop
+sed -i "11c Name[es_ES]=Abrir DV4mini" ~/Desktop/DV4mini.desktop
 
 sudo su | ps aux | grep dv4mini.exe | grep -v grep | awk '{print $2}' | xargs kill
 sudo killall dv_serial
 sudo killall python
+
--- ejecutar_dv4_05.sh
+++ ejecutar_dv4_05.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sed -i "6c Exec=mate-terminal -x sh cerrar_dv4_05.sh" ~/Desktop/DV4mini.desktop
-sed -i "7c Icon=/home/orangepi/dv4mini/dv4k.png" ~/Desktop/DV4mini.desktop
-sed -i "11c Name[es_ES]=Abrir DV4mini" ~/Desktop/DV4mini.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DV4_ON.png" ~/Desktop/DV4mini.desktop
+sed -i "11c Name[es_ES]=Cerrar DV4mini" ~/Desktop/DV4mini.desktop
 
 cd ~/dv4mini
 mono dv4mini.exe
