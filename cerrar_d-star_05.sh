5f13ceb6a9d0b40eb68c0e1983d7ebc4fd4d6cfe
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-03 22:47:11 +0200

    1

--- cerrar_d-star_05.sh
+++ cerrar_d-star_05.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sed -i "6c Exec=/home/orangepi/SCRIPTS_ORANGE/ejecutar_d-star_05.sh" ~/Desktop/D-Star.desktop
+sed -i "6c Exec=/home/orangepi/SCRIPTS_ORANGE/sh ejecutar_d-star_05.sh" ~/Desktop/D-Star.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DSTAR.png" ~/Desktop/D-Star.desktop
 
 #cierra ircDDBGateway
