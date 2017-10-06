6264aff591efd589bf27adb1c054ff42b23d458e
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-06 23:43:22 +0200

    1

--- cerrar_svxlink_05.sh
+++ cerrar_svxlink_05.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i "6c Exec=mate-terminal -x sh ejecutar_svxlink_05.sh" ~/Desktop/SVXLink.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SVXLINK.png" ~/Desktop/SVXLink.desktop
-sed -i "11c Name[es_ES]=Abrir SVXLINK" ~/Desktop/SVXLink.desktop
+sed -i "4c Name[es_ES]=Abrir SVXLINK" ~/Desktop/SVXLink.desktop
 
 
 
