3ccf4550ca3d9b53f1c318067ac1527db022d700
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-06 19:59:34 +0200

    1

--- ejecutar_ysf_05.sh
+++ ejecutar_ysf_05.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
-status6=`grep "BlueDV" /home/pi/status.ini`
-                        status7=`expr substr $status6 8 2`
-#====================================   
-                     
-                        if [ $status7 = "ON" ]
-                        then
- cd /home/pi/V30                       
-lxterminal --geometry=80x7 -e sudo sh menu_ejecutar_d-starrepeater_30.sh
 
-
-
-else
-	sleep 5
  #=========================
-sed -i "4c Exec=sh -c 'cd /home/pi/V30;lxterminal --geometry=72x15 -e sudo sh cerrar_ysf_30.sh'" /home/pi/Desktop/YSFGateway
-sed -i "5c Icon=/home/pi/V30/YSF_ON.png" /home/pi/Desktop/YSFGateway
-sed -i "10c Name[es_ES]=Cerrar YSF" /home/pi/Desktop/YSFGateway
+#sed -i "4c Exec=sh -c 'cd ~/V30;lxterminal --geometry=72x15 -e sudo sh cerrar_ysf_30.sh'" ~/Desktop/YSFGateway
+#sed -i "5c Icon=~/V30/YSF_ON.png" ~/Desktop/YSFGateway
+#sed -i "10c Name[es_ES]=Cerrar YSF" ~/Desktop/YSFGateway
+#sed -i "3c YSF=ON" ~/status.ini
 
-sed -i "3c YSF=ON" /home/pi/status.ini
-cd /home/pi/YSFClients/YSFGateway
+cd ~/YSFClients/YSFGateway
 echo "\33[1;32m"
 clear
 echo "***********************************************************"
@@ -29,6 +17,4 @@ echo "\33[1;33m"
 sudo ./YSFGateway YSFGateway.ini
 #=========================
 
-
-fi
 
