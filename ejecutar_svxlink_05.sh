6a9fe4d527e30762d21488ee1300afe2c35e7347
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-06 23:33:20 +0200

    1

--- ejecutar_svxlink_05.sh
+++ ejecutar_svxlink_05.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-#sed -i "4c Exec=sh -c 'cd /home/pi/V30;lxterminal --geometry=72x15 -e sudo sh cerrar_svxlink_30.sh'" /home/pi/Desktop/svxlink
-#sed -i "5c Icon=/home/pi/V30/SVXLINK_ON.png" /home/pi/Desktop/svxlink
-#sed -i "10c Name[es_ES]=Cerrar SVXLINK" /home/pi/Desktop/svxlink
+sed -i "6c Exec=mate-terminal -x sh cerrar_svxlink_05.sh" /home/pi/Desktop/SVXLink.desktop
+sed -i "7c Icon=/home/pi/V30/SVXLINK_ON.png" /home/pi/Desktop/SVXLink.desktop
+sed -i "11c Name[es_ES]=Cerrar SVXLINK" /home/pi/Desktop/SVXLink.desktop
 
-
+    #[Desktop Entry]
+    #Version=1.0
+    #Type=Application
+    #Name=YSFGateway
+    #Comment=
+    #Exec=mate-terminal -x sh ejecutar_ysf_05.sh
+    #Icon=/home/orangepi/SCRIPTS_ORANGE/C4FM.png
+    #Path=/home/orangepi/SCRIPTS_ORANGE
+    #Terminal=false
+    #StartupNotify=false
+    #Name[es_ES]=Abrir YSF
 sudo svxlink
