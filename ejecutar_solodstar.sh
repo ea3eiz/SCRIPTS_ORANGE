d251b2cfb8b884ddf2aeaa744947538b8e9f4a6a
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-08 00:29:06 +0200

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-echo "\33[1;37m"
-cd ~/MMDVMHost
-echo "\33[1;32m"
-clear
-echo "***********************************************"
-echo "*                 Dstar solo                 * "
-echo "***********************************************"
-#sleep 2
-echo "\33[1;37m"
+
 sudo killall MMDVMDSTAR
+cd MMDVMHost
 sudo mate-terminal -x ./MMDVMDSTAR MMDVMBM.ini & ircddbgateway -gui
 
