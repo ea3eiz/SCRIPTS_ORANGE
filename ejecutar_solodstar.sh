d31b5415d74c3e7e2932829d548a313fc37f1bb7
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-08 00:22:19 +0200

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sed -i "6c Exec=mate-terminal -x sh cerrar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMRPLUS_ON.png" ~/Desktop/DMRPLUS.desktop
-sed -i "10c Name[es_ES]=Cerrar DMR+" ~/Desktop/DMRPLUS.desktop
-
-
 echo "\33[1;37m"
 cd ~/MMDVMHost
 echo "\33[1;32m"
