9aa483aa0eac0b4c4aa300bc0e2a00a34e290778
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-07 10:24:04 +0200

    1

--- actualizar_IDS_MMDVMHost_05.sh
+++ actualizar_IDS_MMDVMHost_05.sh
@@ -4,3 +4,4 @@ echo "\33[1;32m ACTUALIZANDO IDS INDICATIVOS >>>>>"
 sed -i "52c DMRIDFILE=~/MMDVMHost/DMRIds.dat" ~/MMDVMHost/linux/DMRIDUpdate.sh
 sed -i '58c #RESTARTCOMMAND="systemctl restart mmdvmhost.service' ~/MMDVMHost/linux/DMRIDUpdate.sh
 sudo sh ~/MMDVMHost/linux/DMRIDUpdate.sh
+sleep 10
