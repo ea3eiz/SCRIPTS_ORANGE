2a70aa1e96a1540a89ee8ad04b0421dc6277ea30
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-07 10:06:01 +0200

    1

--- actualizar_IDS_MMDVMHost_05.sh
+++ actualizar_IDS_MMDVMHost_05.sh
@@ -2,6 +2,6 @@
 clear
 echo "\33[1;32m ACTUALIZANDO IDS INDICATIVOS >>>>>"
 sleep 10
-sed -i "52c DMRIDFILE=/home/pi/MMDVMHost/DMRIds.dat" /home/pi/MMDVMHost/DMRIDUpdate.sh
-sed -i '58c #RESTARTCOMMAND="systemctl restart mmdvmhost.service' /home/pi/MMDVMHost/DMRIDUpdate.sh
-sudo sh /home/pi/MMDVMHost/DMRIDUpdate.sh
+sed -i "52c DMRIDFILE=~/MMDVMHost/DMRIds.dat" ~/MMDVMHost/DMRIDUpdate.sh
+sed -i '58c #RESTARTCOMMAND="systemctl restart mmdvmhost.service' ~/MMDVMHost/DMRIDUpdate.sh
+sudo sh ~/MMDVMHost/DMRIDUpdate.sh
