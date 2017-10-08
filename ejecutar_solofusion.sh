7116aac53e85aaa8d3c6138f86fe76f844d78d7a
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-09 00:19:34 +0200

    1

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -13,7 +13,7 @@ sudo mate-terminal -x ./YSFGateway YSFGateway.ini & cd ~/MMDVMHost
 sudo mate-terminal -x ./MMDVMFUSION MMDVMFUSION.ini
 
 
-sed -i "6c Exec=mate-terminal -x sh ejecutar_ysf_05.sh" ~/Desktop/YSFGateway.desktop
+sed -i "6c Exec=mate-terminal -x sh ejecutar_solofusion.sh" ~/Desktop/YSFGateway.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/C4FM.png" ~/Desktop/YSFGateway.desktop
 sed -i "11c Name[es_ES]=Abrir YSF" ~/Desktop/YSFGateway.desktop
 
