8a4624a80b5cfb1bb79f9c2ce9e7e8c65729766c
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-08 20:05:47 +0200

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "6c Exec=sudo sh cerrar_solodstar.sh" ~/Desktop/Abrir_SVXLink.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" ~/Desktop/Abrir_SVXLink.desktop
-sed -i "4c Name[es_ES]=Cerrar solo D-STAR" ~/Desktop/Abrir_SVXLink.desktop
+#sed -i "6c Exec=sudo sh cerrar_solodstar.sh" ~/Desktop/Abrir_SVXLink.desktop
+#sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" ~/Desktop/Abrir_SVXLink.desktop
+#sed -i "4c Name[es_ES]=Cerrar solo D-STAR" ~/Desktop/Abrir_SVXLink.desktop
 
 
 cd ~/MMDVMHost
