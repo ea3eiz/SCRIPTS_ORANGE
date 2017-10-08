8a8ff6106f1ce3ec8653815fec889eae7189eca5
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-08 20:34:08 +0200

    1

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sed -i "6c Exec=sudo sh ejecutar_solodstar.sh" ~/Desktop/AbrirsoloDstar.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" ~/Desktop/AbrirsoloDstar.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR.png" ~/Desktop/AbrirsoloDstar.desktop
 sed -i "4c Name[es_ES]=Abrir solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
 
-sudo killall MMDVMDSTAR & ircddbgateway
+sudo killall MMDVMDSTAR
+sudo killall ircddbgateway
