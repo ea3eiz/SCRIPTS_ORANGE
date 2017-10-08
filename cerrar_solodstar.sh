d5650c6d53ae8f9fd7eaac5af5c74ae8712519da
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-08 20:02:54 +0200

    1

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -4,6 +4,4 @@ sed -i "6c Exec=sudo sh cerrar_solodstar.sh" ~/Desktop/Abrir_SVXLink.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" ~/Desktop/Abrir_SVXLink.desktop
 sed -i "4c Name[es_ES]=Cerrar solo D-STAR" ~/Desktop/Abrir_SVXLink.desktop
 
-
-cd ~/MMDVMHost
-sudo mate-terminal -x ./MMDVMDSTAR MMDVMDSTAR.ini & ircddbgateway -gui
+sudo killall MMDVMDSTAR & ircddbgateway
