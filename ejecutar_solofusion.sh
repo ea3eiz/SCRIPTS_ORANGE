0fb1056953b3dc5e2e741e5885c06fb036329ade
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-09 00:30:26 +0200

    1

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -14,6 +14,6 @@ sudo mate-terminal -x ./MMDVMFUSION MMDVMFUSION.ini
 
 
 sed -i "6c Exec=sudo sh ejecutar_solofusion.sh" ~/Desktop/AbrirsoloYSF.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/C4FM.png" ~/Desktop/AbrirsoloYSF.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_YSF.png" ~/Desktop/AbrirsoloYSF.desktop
 sed -i "11c Name[es_ES]=Abrir YSF" ~/Desktop/AbrirsoloYSF.desktop
 
