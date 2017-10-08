7389fb09deeec1c51bda9a78641c2bf18ba312e4
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-09 00:36:43 +0200

    1

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 sed -i "6c Exec=sudo sh cerrar_solofusion.sh" ~/Desktop/AbrirsoloYSF.desktop
-sleep 1
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_YSF_ON.png" ~/Desktop/AbrirsoloYSF.desktop
-sleep 1
 sed -i "11c Name[es_ES]=Cerrar solo FUSION" ~/Desktop/AbrirsoloYSF.desktop
-sleep 1
 
 
 cd ~/YSFClients/YSFGateway
@@ -13,7 +10,7 @@ sudo mate-terminal -x ./YSFGateway YSFGateway.ini & cd ~/MMDVMHost
 sudo mate-terminal -x ./MMDVMFUSION MMDVMFUSION.ini
 
 
-sed -i "6c Exec=sudo sh ejecutar_solofusion.sh" ~/Desktop/AbrirsoloYSF.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_YSF.png" ~/Desktop/AbrirsoloYSF.desktop
-sed -i "11c Name[es_ES]=Abrir YSF" ~/Desktop/AbrirsoloYSF.desktop
+#sed -i "6c Exec=sudo sh ejecutar_solofusion.sh" ~/Desktop/AbrirsoloYSF.desktop
+#sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_YSF.png" ~/Desktop/AbrirsoloYSF.desktop
+#sed -i "11c Name[es_ES]=Abrir YSF" ~/Desktop/AbrirsoloYSF.desktop
 
