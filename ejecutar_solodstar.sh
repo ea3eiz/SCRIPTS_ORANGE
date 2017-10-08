790c13ed9b049d68ea65360cc2e264a8070f299b
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-09 01:51:16 +0200

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -2,7 +2,7 @@
 
 sed -i "6c Exec=sudo sh cerrar_solodstar.sh" ~/Desktop/AbrirsoloDstar.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" ~/Desktop/AbrirsoloDstar.desktop
-sed -i "4c Name[es_ES]=Cerrar solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
+sed -i "11c Name[es_ES]=Cerrar solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
 
 
 cd ~/MMDVMHost
