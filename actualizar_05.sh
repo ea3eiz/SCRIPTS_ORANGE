917b0a56549e5308f954cbefb5504dc8767dcbb4
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-08 09:42:11 +0200

    1

--- actualizar_05.sh
+++ actualizar_05.sh
@@ -31,7 +31,7 @@
 
 sed -i "6c Exec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
-sed -i "10c Name[es_ES]=eas4aoj" ~/Desktop/BM.desktop
+sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/BM.desktop
 
 # quita el botón verde del DMR+ al iniciar si se hubiera quedado encendido
 sed -i "6c Exec=mate-terminal -x sh ejecutar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
