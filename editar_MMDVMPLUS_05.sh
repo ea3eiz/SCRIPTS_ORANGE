4f0252f0ec43ff3b8e8b5784d2b98a09f5e4d6cb
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-10 23:16:30 +0200

    1

--- editar_MMDVMPLUS_05.sh
+++ editar_MMDVMPLUS_05.sh
@@ -401,7 +401,7 @@ done;;
 while true
 do
 echo "Valor actual del Master: \33[1;33m${master#*=}\33[1;37m"
-                      read -p 'Brandmeister=master.spain-dmr.es / DMR+=eaglobalmaster.xreflector.es: ' master1
+                      read -p 'DMR+=eamaster04.xreflector.es  ó  eaglobalmaster.xreflector.es: ' master1
                           actualizar=S 
                           case $actualizar in
                     [sS]* ) echo ""
