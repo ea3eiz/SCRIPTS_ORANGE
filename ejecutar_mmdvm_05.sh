bb1e6675d80bcb462881d81713271da88f5f76d9
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-10 12:34:54 +0200

    1

--- ejecutar_mmdvm_05.sh
+++ ejecutar_mmdvm_05.sh
@@ -3,7 +3,16 @@
 #sed -i "5c Icon=~/V30/MMDVM_ON.png" ~/Desktop/Abrir_Radio
 #sed -i "10c Name[es_ES]=Cerrar Radio" ~/Desktop/Abrir_Radio
 
-#sed -i "5c MMDVM=ON" ~/status.ini
+
+
+sed -i "6 cExec=mate-terminal -x sh cerrar_mmdvm_05.sh" ~/Desktop/MMDVM.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BRANDMEISTER_ON.png" ~/Desktop/MMDVM.desktop
+sed -i "4c Name[es_ES]=Cerrar Radio" ~/Desktop/MMDVM.desktop
+
+
+
+
+
 cd ~/MMDVMHost
 
 echo "\33[1;32m"
