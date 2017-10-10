0fc4755d5dd7a8bbdf72d82abcd05a787b3c72cb
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-10 12:40:03 +0200

    1

--- cerrar_mmdvm_05.sh
+++ cerrar_mmdvm_05.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo killall MMDVMHost
-sed -i "5c MMDVM=OFF" /home/pi/status.ini
+sed -i "6 cExec=mate-terminal -x sh ejecutar_mmdvm_05.sh" ~/Desktop/MMDVM.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/MMDVM.png" ~/Desktop/MMDVM.desktop
+sed -i "4c Name[es_ES]=Abrir Radio" ~/Desktop/MMDVM.desktop
 
-sed -i "4cExec=sh -c 'cd /home/pi/V30; lxterminal --geometry=72x15 -e sudo sh ejecutar_mmdvm_30.sh'" /home/pi/Desktop/Abrir_Radio
-sed -i "5c Icon=/home/pi/V30/MMDVM.png" /home/pi/Desktop/Abrir_Radio
-sed -i "10c Name[es_ES]=Abrir Radio" /home/pi/Desktop/Abrir_Radio
+sudo killall MMDVMHost
--- ejecutar_mmdvm_05.sh
+++ ejecutar_mmdvm_05.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-#sed -i "4c Exec=sh -c 'cd ~/V30;lxterminal --geometry=72x15 -e sudo sh cerrar_mmdvm_30.sh'" ~/Desktop/Abrir_Radio
-#sed -i "5c Icon=~/V30/MMDVM_ON.png" ~/Desktop/Abrir_Radio
-#sed -i "10c Name[es_ES]=Cerrar Radio" ~/Desktop/Abrir_Radio
-
-
-
 sed -i "6 cExec=mate-terminal -x sh cerrar_mmdvm_05.sh" ~/Desktop/MMDVM.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/MMDVM_ON.png" ~/Desktop/MMDVM.desktop
 sed -i "4c Name[es_ES]=Cerrar Radio" ~/Desktop/MMDVM.desktop
-
-
-
-
 
 cd ~/MMDVMHost
 
