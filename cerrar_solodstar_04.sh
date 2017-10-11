ad062c70a5b62b3e38c4165fb51c0bae35cd7ca8
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-11 02:07:21 +0200

    1

--- cerrar_solodstar_04.sh
+++ cerrar_solodstar_04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i "6c Exec=mate-terminal -x sh ejecutar_solodstar_04.sh" ~/Desktop/Dstar_solo.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/Dstar_solo.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_DSTAR.png" ~/Desktop/Dstar_solo.desktop
 sed -i "10c Name[es_ES]=Abrir D-STAR Repeater" ~/Desktop/Dstar_solo.desktop
 
 
