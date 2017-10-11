386c5f47ff9f685300e907241d3609166d416b7b
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-11 02:22:12 +0200

    1

--- cerrar_solodstar_04.sh
+++ cerrar_solodstar_04.sh
@@ -4,9 +4,5 @@ sed -i "6c Exec=mate-terminal -x sh ejecutar_solodstar_04.sh" ~/Desktop/Dstar_so
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_DSTAR.png" ~/Desktop/Dstar_solo.desktop
 sed -i "10c Name[es_ES]=Abrir D-STAR Repeater" ~/Desktop/Dstar_solo.desktop
 
-
-
-#cierra ircDDBGateway
 sudo killall ircddbgateway
-#cierra D-STARRepeater
 sudo killall dstarrepeater
