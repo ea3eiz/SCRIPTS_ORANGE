30a70866b92123b06b67b7ec0ecc7130a2d90e36
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-11 20:49:23 +0200

    1

--- menu_extra_05.sh
+++ menu_extra_05.sh
@@ -8,13 +8,13 @@ echo "   *      Script para hacer actualizaciones y varios \33[1;33mV.02.05     
 echo "   *                 \33[1;31mby EA3EIZ & EA4AOJ\33[1;32m                               *"
 echo "   ********************************************************************"
 echo "\33[1;36m   1)\33[1;37m Actualizar imagen"
-echo "\33[1;36m   2)\33[1;37m Restaurar iconos originales"
-echo "\33[1;36m   3)\33[1;37m Copiar Tarjeta SD a la memoria interna EMMC"
+#echo "\33[1;36m   2)\33[1;37m Restaurar iconos originales"
+echo "\33[1;36m   2)\33[1;37m Copiar Tarjeta SD a la memoria interna EMMC"
 #echo "\33[1;36m   2)\33[1;37m Actualizar DV4mini Control Panel (Stick ID:8D-6E-8C V1,77)"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
 echo ""
-echo -n "\33[1;36m   Elige una opción del 0 al 3: " 
+echo -n "\33[1;36m   Elige una opción del 0 al 2: " 
 read escoger_menu
 echo ""
 case $escoger_menu in
@@ -40,29 +40,8 @@ exit;
 break;;
 esac
 done;;
+
 2) echo ""
-while true
-do
-clear
-	                    ejecutar1=S
-		                case $ejecutar1 in
-			            [sS]* ) echo ""
-			            echo ">>>>>>>>> RESTAURANDO ICONOS ESCRITORIO>>>>>>>>"
-			            cd ~/SCRIPTS_ORANGE
-                        git pull
-                        sleep 3
-                        cp -R ~/SCRIPTS_ORANGE/Desktop ~/
-                        chmod +x ~/Desktop
-                        clear
-                        exit;
-		                break;;
-		                [nN]* ) echo ""
-clear
-exit;
-break;;
-esac
-done;;
-3) echo ""
 while true
 do
 clear
@@ -142,6 +121,28 @@ clear
                         exit;
 						break;;
 						[nN]* ) echo ""
+clear
+exit;
+break;;
+esac
+done;;
+4000) echo ""
+while true
+do
+clear
+	                    ejecutar1=S
+		                case $ejecutar1 in
+			            [sS]* ) echo ""
+			            echo ">>>>>>>>> RESTAURANDO ICONOS ESCRITORIO>>>>>>>>"
+			            cd ~/SCRIPTS_ORANGE
+                        git pull
+                        sleep 3
+                        cp -R ~/SCRIPTS_ORANGE/Desktop ~/
+                        chmod +x ~/Desktop
+                        clear
+                        exit;
+		                break;;
+		                [nN]* ) echo ""
 clear
 exit;
 break;;
