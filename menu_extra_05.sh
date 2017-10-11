4c550691cdc4f2ba1bdb424e3e451b8ecdab6bdb
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-11 10:12:32 +0200

    1

--- menu_extra_05.sh
+++ menu_extra_05.sh
@@ -23,83 +23,76 @@ while true
 do
 clear
 
-	                ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
+	                    ejecutar1=S
+		                case $ejecutar1 in
+			            [sS]* ) echo ""
+			            echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
                         cd ~/SCRIPTS_ORANGE
                         git pull 
-                          sleep 3
+                        sleep 3
                         cd ~/
                         sudo chmod 777 SCRIPTS_ORANGE
-
                         clear
                         exit;
-		break;;
-		[nN]* ) echo ""
+		                break;;
+						[nN]* ) echo ""
 clear
 exit;
-			break;;
+break;;
 esac
 done;;
-
-
 2) echo ""
 while true
 do
 clear
-
-	                ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo ">>>>>>>>> RESTAURANDO ICONOS >>>>>>>>"
+	                    ejecutar1=S
+		                case $ejecutar1 in
+			            [sS]* ) echo ""
+			            echo ">>>>>>>>> RESTAURANDO ICONOS ESCRITORIO>>>>>>>>"
 			            cd ~/SCRIPTS_ORANGE
                         git pull
+                        sleep 3
                         cp -R ~/SCRIPTS_ORANGE/Desktop ~/
                         chmod +x ~/Desktop
                         clear
                         exit;
-		break;;
-		[nN]* ) echo ""
+		                break;;
+		                [nN]* ) echo ""
 clear
 exit;
-			break;;
+break;;
 esac
 done;;
-
 3) echo ""
 while true
 do
 clear
-
-	        ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo ">>>>>>>>> COPIAR TARJETA   >>>>>"
+	                    ejecutar1=S
+		                case $ejecutar1 in
+			            [sS]* ) echo ""
+			            echo ">>>>>>>>> COPIAR TARJETA MSD EN EMMC   >>>>>"
 			            sudo nand-sata-install
                         exit;
-		break;;
-		[nN]* ) echo ""
+		                break;;
+		                [nN]* ) echo ""
 clear
 exit;
-			break;;
+break;;
 esac
 done;;
-
 1000) echo ""
 while true
 do
 clear
-
-            echo "\33[1;31m" #color rojo
-            echo " *****************************************************"
-            echo " ******   OJO!!! ESTO RESTAURA TODO EL GITHUB ********"
-            echo " *****************************************************"
-            echo "\33[1;37m" #color
-            read -p 'Quieres Actualizar? Si/No: ' ejecutar1
-            case $ejecutar1 in
-			[sS]* ) echo ""
-			echo ">>>>>>>>> RESTAURANDO SISTEMA >>>>>>>>"
+                        echo "\33[1;31m" #color rojo
+                        echo " *****************************************************"
+                        echo " ******   OJO!!! ESTO RESTAURA TODO EL GITHUB ********"
+                        echo " *****************************************************"
+                        echo "\33[1;37m" #color
+                        read -p 'Quieres Actualizar? Si/No: ' ejecutar1
+                        case $ejecutar1 in
+			            [sS]* ) echo ""
+			            echo ">>>>>>>>> RESTAURANDO SISTEMA >>>>>>>>"
 			            sudo rm -R ~/SCRIPTS_ORANGE
                         cd ~
                         git clone https://github.com/ea3eiz/SCRIPTS_ORANGE
@@ -107,44 +100,39 @@ clear
                         chmod +x ~/Desktop
                         clear
                         exit;
-		break;;
-		[nN]* ) echo ""
+						break;;
+						[nN]* ) echo ""
 clear
 exit;
-			break;;
+break;;
 esac
 done;;
-
-
 2000) echo ""
 while true
 do
 clear
-
-	        ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo ">>>>>>>>> GIT PULL AUTOSTART  recoge los botones autoarranque >>>>>"
+	        			ejecutar1=S
+		    			case $ejecutar1 in
+						[sS]* ) echo ""
+						echo ">>>>>>>>> GIT PULL AUTOSTART  recoge los botones nuevos autoarranque >>>>>"
 			            cd ~/AUTOSTART
 			            git pull
                         exit;
-		break;;
-		[nN]* ) echo ""
+						break;;
+						[nN]* ) echo ""
 clear
 exit;
-			break;;
+break;;
 esac
 done;;
-
 3000) echo ""
 while true
 do
 clear
-
-	        ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo ">>>>>>>>> INSTALANDO BMXTG   >>>>>"
+	        			ejecutar1=S
+		    			case $ejecutar1 in
+						[sS]* ) echo ""
+						echo ">>>>>>>>> INSTALANDO BMXTG   >>>>>"
 			            cd ~/
 			            sudo dpkg -i bmxtg-1.3.deb
 			            sudo apt-get install python-requests -y
@@ -153,295 +141,13 @@ clear
 			            cd ~/bmxtg
 			            sudo chmod +x bmxtg.py
                         exit;
-		break;;
-		[nN]* ) echo ""
+						break;;
+						[nN]* ) echo ""
 clear
 exit;
-			break;;
+break;;
 esac
 done;;
-
-#31) echo ""
-#while true
-#do
-#clear
-	                       # ejecutar1=S
-		            #case $ejecutar1 in
-			#[sS]* ) echo ""
-			#echo "ok >>>>>"
-                                   # sed -i "4c Exec=sh -c """ /home/pi/Desktop/Boton_1
-                                   # sed -i "5c Icon=/home/pi/V30/OCULTO.png" /home/pi/Desktop/Boton_1
-                                   # sed -i "10c Name[es_ES]=." /home/pi/Desktop/Boton_1
-		           # echo ""
-			#echo "Ok, se ha ejecutado correctamente"
-			#echo ""
-			#break;;
-			#[nN]* ) echo ""
-			#break;;
-#esac
-#done;;
-32) echo ""
-while true
-do
-clear
-	                        ejecutar1=S
-		            case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "ok >>>>>"
-                                    sed -i "4c Exec=sh -c """ /home/pi/Desktop/Boton_2
-                                    sed -i "5c Icon=/home/pi/V30/OCULTO.png" /home/pi/Desktop/Boton_2
-                                    sed -i "10c Name[es_ES]=." /home/pi/Desktop/Boton_2
-		            echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-33) echo ""
-while true
-do
-clear
-	                        ejecutar1=S
-		            case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "ok >>>>>"
-                                    sed -i "4c Exec=sh -c """ /home/pi/Desktop/Boton_3
-                                    sed -i "5c Icon=/home/pi/V30/OCULTO.png" /home/pi/Desktop/Boton_3
-                                    sed -i "10c Name[es_ES]=." /home/pi/Desktop/Boton_3
-		            echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-34) echo ""
-while true
-do
-clear
-	                        ejecutar1=S
-		            case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "ok >>>>>"
-                                    sed -i "4c Exec=sh -c """ /home/pi/Desktop/Boton_4
-                                    sed -i "5c Icon=/home/pi/V30/OCULTO.png" /home/pi/Desktop/Boton_4
-                                    sed -i "10c Name[es_ES]=." /home/pi/Desktop/Boton_4
-  		            echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-#41) echo ""
-#while true
-#do
-#clear
-	                        #ejecutar1=S
-		            #case $ejecutar1 in
-			#[sS]* ) echo ""
-			#echo "ok >>>>>"
-			#cd /home/pi/MMDVMHost
-			#sudo cp MMDVMHost MMDVMSOLODSTAR
-			#sudo cp MMDVMBM.ini MMDVMSOLODSTAR.ini
-                                    #sed -i "4c Exec=sh -c 'cd /home/pi/V30/;sudo sh ejecutar_solodstar_04.sh'" /home/pi/Desktop/Boton_1
-                                    #sed -i "5c Icon=/home/pi/V30/SOLO_DSTAR.png" /home/pi/Desktop/Boton_1
-                                    #sed -i "10c Name[es_ES]=Abrir solo D_STAR" /home/pi/Desktop/Boton_1
-                                    #sed -i "9c Terminal=true" /home/pi/Desktop/Boton_1
-		            #echo ""
-			#echo "Ok, se ha ejecutado correctamente"
-			#echo ""
-			#break;;
-			#[nN]* ) echo ""
-			#break;;
-#esac
-#done;;
-42) echo ""
-while true
-do
-clear
-	                        ejecutar1=S
-		            case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "ok >>>>>"
-                                    sed -i "4c Exec=sh -c 'cd /home/pi/V30/;lxterminal --geometry=80x47 -e sudo sh actualizar_IDS_MMDVMHost.sh'" /home/pi/Desktop/Boton_2
-                                    sed -i "5c Icon=/home/pi/V30/EDITAR.png" /home/pi/Desktop/Boton_2
-                                    sed -i "10c Name[es_ES]=Actualizar IDS MMDVMHost" /home/pi/Desktop/Boton_2
-		            echo ""0
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-43) echo ""
-while true
-do
-clear
-	                        ejecutar1=S
-		            case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "ok >>>>>"
-                                    sed -i "4c Exec=sh -c """ /home/pi/Desktop/Boton_3
-                                    sed -i "5c Icon=/home/pi/V30/AUTOARRANQUE.png" /home/pi/Desktop/Boton_3
-                                    sed -i "10c Name[es_ES]=." /home/pi/Desktop/Boton_3
-		            echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-44) echo ""
-while true
-do
-clear
-	                        ejecutar1=S
-		            case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "ok >>>>>"
-                                    sed -i "4c Exec=sh -c 'cd /home/pi/V30;sudo sh actualizar_YSF_30.sh'" /home/pi/Desktop/Boton_4
-                                    sed -i "5c Icon=/home/pi/V30/ACTUALIZAR_BD.png" /home/pi/Desktop/Boton_4
-                                    sed -i "10c Name[es_ES]=Actualizar YSF" /home/pi/Desktop/Boton_4
-		            echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-
-6000) echo ""
-while true
-do
-clear
-		    ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "ok poniendo beep del echolink >>>>>"
-			sleep 3
-			sed -i '515c if {!$is_open} {' /usr/local/share/svxlink/events.d/EchoLink.tcl
-			sed -i '516c playSilence 200' /usr/local/share/svxlink/events.d/EchoLink.tcl
-			sed -i '517c playTone 1000 100 100' /usr/local/share/svxlink/events.d/EchoLink.tcl
-			sed -i '518c }' /usr/local/share/svxlink/events.d/EchoLink.tcl
-			echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-7000) echo ""
-while true
-do
-clear
-		    ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "Haciendo copias de los editores BM, DMR+, LIBRE, RADIO y SVXLINK >>>>>"
-			sleep 3
-			sudo chmod -R 777 /home/pi/Downloads
-            cd /home/pi/MMDVMHost
-            sudo cp -f MMDVM.ini /home/pi/Downloads
-            sudo cp -f MMDVM.ini_copia /home/pi/Downloads
-            sudo cp -f MMDVM.ini_copia2 /home/pi/Downloads
-            sudo cp -f MMDVM.ini_copia3 /home/pi/Downloads
-            sudo cp -f MMDVM.ini_original /home/pi/Downloads
-            sudo cp -f MMDVMBM.ini /home/pi/Downloads
-            sudo cp -f MMDVMBM.ini_copia /home/pi/Downloads
-            sudo cp -f MMDVMBM.ini_copia2 /home/pi/Downloads
-            sudo cp -f MMDVMBM.ini_copia3 /home/pi/Downloads
-            sudo cp -f MMDVMLIBRE.ini /home/pi/Downloads
-            sudo cp -f MMDVMLIBRE.ini_uno /home/pi/Downloads
-            sudo cp -f MMDVMLIBRE.ini_dos /home/pi/Downloads       
-            sudo cp -f MMDVMPLUS.ini /home/pi/Downloads
-            sudo cp -f MMDVMPLUS.ini_copia /home/pi/Downloads
-            sudo cp -f MMDVMPLUS.ini_copia2 /home/pi/Downloads
-            sudo cp -f MMDVMPLUS.ini_copia3 /home/pi/Downloads
-
-            cd /usr/local/etc/svxlink/
-            sudo cp -f svxlink.conf /home/pi/Downloads
-
-            cd /usr/local/etc/svxlink/svxlink.d/
-            sudo cp -f ModuleEchoLink.conf /home/pi/Downloads
-			echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-8000) echo ""
-while true
-do
-clear
-		    ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "Restaurando copias de los editores BM, DMR+, LIBRE, RADIO y SVXLINK   >>>>>"
-			sleep 3
-			sudo chmod -R 777 /home/pi/Downloads
-            cd /home/pi/Downloads
-            sudo cp -f MMDVM.ini /home/pi/MMDVMHost
-            sudo cp -f MMDVM.ini_copia /home/pi/MMDVMHost
-            sudo cp -f MMDVM.ini_copia2 /home/pi/MMDVMHost
-            sudo cp -f MMDVM.ini_copia3 /home/pi/MMDVMHost
-            sudo cp -f MMDVM.ini_original /home/pi/MMDVMHost
-            sudo cp -f MMDVMBM.ini /home/pi/MMDVMHost
-            sudo cp -f MMDVMBM.ini_copia /home/pi/MMDVMHost
-            sudo cp -f MMDVMBM.ini_copia2 /home/pi/MMDVMHost
-            sudo cp -f MMDVMBM.ini_copia3 /home/pi/MMDVMHost
-            sudo cp -f MMDVMLIBRE.ini /home/pi/MMDVMHost
-            sudo cp -f MMDVMLIBRE.ini_uno /home/pi/MMDVMHost
-            sudo cp -f MMDVMLIBRE.ini_dos /home/pi/DoMMDVMHost     
-            sudo cp -f MMDVMPLUS.ini /home/pi/MMDVMHost
-            sudo cp -f MMDVMPLUS.ini_copia /home/pi/MMDVMHost
-            sudo cp -f MMDVMPLUS.ini_copia2 /home/pi/MMDVMHost
-            sudo cp -f MMDVMPLUS.ini_copia3 /home/pi/MMDVMHost
-
-            sudo cp -f svxlink.conf /usr/local/etc/svxlink/
-            
-            sudo cp -f ModuleEchoLink.conf /usr/local/etc/svxlink/svxlink.d/
-
-			echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-
-9000) echo ""
-while true
-do
-clear
-		    ejecutar1=S
-		    case $ejecutar1 in
-			[sS]* ) echo ""
-			echo "Copiando fichero solodstar.desktop para el autoarranque >>>>>"
-			sleep 3
-			cd /home/pi/V30
-            sudo cp -f solodstar.desktop /home/pi/
-            echo ""
-			echo "Ok, se ha ejecutado correctamente"
-			echo ""
-			break;;
-			[nN]* ) echo ""
-			break;;
-esac
-done;;
-
-
 0) echo ""
 clear
 echo "\33[1;33m   ******************************"
