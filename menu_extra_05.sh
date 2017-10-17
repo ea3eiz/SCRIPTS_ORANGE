ffafb4041c4a8651d71074dfce17231721f15e8b
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-18 01:37:40 +0200

    1

--- menu_extra_05.sh
+++ menu_extra_05.sh
@@ -90,15 +90,15 @@ clear
                         case $ejecutar1 in
 			            [sS]* ) echo ""
 			            echo ">>>>>>>>> RESTAURANDO SISTEMA >>>>>>>>"
-			            #sudo rm -R ~/SCRIPTS_ORANGE
+			            sudo rm -R ~/SCRIPTS_ORANGE
+                        cd ~
+                        git clone https://github.com/ea3eiz/SCRIPTS_ORANGE
+                        cp -R ~/SCRIPTS_ORANGE/Desktop ~/
+                        chmod +x ~/Desktop
+                        #sudo rm -R ~/AUTOSTART
                         #cd ~
-                        #git clone https://github.com/ea3eiz/SCRIPTS_ORANGE
-                        #cp -R ~/SCRIPTS_ORANGE/Desktop ~/
-                        #chmod +x ~/Desktop
-                        sudo rm -R ~/AUTOSTART
-                        #cd ~
-                        git clone https://github.com/ea3eiz/AUTOSTART
-                        chmod +x ~/AUTOSTART
+                        #git clone https://github.com/ea3eiz/AUTOSTART
+                        #chmod +x ~/AUTOSTART
 
 
 
