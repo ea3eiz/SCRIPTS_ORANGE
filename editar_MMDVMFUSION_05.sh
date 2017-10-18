bdfba1bd9ad1fdcfc5c6edd0ed149f34cb92a3d8
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-18 09:22:33 +0200

    1

--- editar_MMDVMFUSION_05.sh
+++ editar_MMDVMFUSION_05.sh
@@ -771,7 +771,7 @@ do
                               actualizar=S 
                               case $actualizar in
                               [sS]* ) echo ""
-                              geany ~/MMDVMHost/MMDVMBM.ini
+                              sudo pluma ~/MMDVMHost/MMDVMBM.ini
                               break;;
                               [nN]* ) echo ""
                               break;;
