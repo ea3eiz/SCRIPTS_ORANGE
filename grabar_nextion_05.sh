dde108192f6ef73a9987904a1b49573d677e6105
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-19 23:53:36 +0200

    1

--- grabar_nextion_05.sh
+++ grabar_nextion_05.sh
@@ -14,7 +14,7 @@ echo "\33[1;36m   4)\33[1;37m Grabar pantalla Nextion 4.3 Pulgadas MMDVM"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
 echo ""
-echo -n "\33[1;36m   Elige una opción del 0 al 8: " 
+echo -n "\33[1;36m   Elige una opción del 0 al 4: " 
 read escoger_menu
 echo ""
 case $escoger_menu in
