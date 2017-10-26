a28184155c316070e0da7bbc342839e882290ba4
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-27 00:00:23 +0200

    1

--- actualizar_05.sh
+++ actualizar_05.sh
@@ -14,3 +14,12 @@ sudo ntpdate -u hora.roa.es
 
 # pone el altavoz en la barra superior
 pasystray &
+
+# PARA QUE MANDE UN MAIL SI SE TIENE PUESTO EL INDICATIVO EN EL dmrplus o brandmeister
+bm=`sed -n '2p'  ~/MMDVMHost/MMDVMBM.ini`
+SUBJECT6=`sed -n '109p'  ~/MMDVMHost/MMDVMBM.ini`
+indicativo=`sed -n '2p'  ~/MMDVMHost/MMDVMPLUS.ini`
+SUBJECT4=`sed -n '109p'  ~/MMDVMHost/MMDVMPLUS.ini`
+
+sudo wget -post-data http://associacioader.com/MAIL_DMRPLUS.php?plus=$indicativo
+sudo wget -post-data http://associacioader.com/MAIL_BRANDMEISTER.php?plus=$bm
