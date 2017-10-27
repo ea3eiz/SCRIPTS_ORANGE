1951088adb9bc744e1c558bd6e27f3a53252949f
ea3eiz <ea3eiz@ea3eiz.com>
2017-10-27 11:35:52 +0200

    1

--- actualizar_05.sh
+++ actualizar_05.sh
@@ -16,13 +16,24 @@ sudo ntpdate -u hora.roa.es
 pasystray &
 
 # PARA QUE MANDE UN MAIL SI SE TIENE PUESTO EL INDICATIVO EN EL dmrplus o brandmeister
+#bm=`sed -n '2p'  ~/MMDVMHost/MMDVMBM.ini`
+#plus=`sed -n '2p'  ~/MMDVMHost/MMDVMPLUS.ini`
+#dstar=`sed -n '2p'  ~/MMDVMHost/MMDVMDSTAR.ini`
+#fusion=`sed -n '2p'  ~/MMDVMHost/MMDVMFUSION.ini`
+
+
+#sudo wget -post-data http://associacioader.com/MAIL_DMRPLUS_O.php?plus=$plus
+#sudo wget -post-data http://associacioader.com/MAIL_BRANDMEISTER_O.php?plus=$bm
+#sudo wget -post-data http://associacioader.com/MAIL_FUSION_O.php?plus=$fusion
+#sudo wget -post-data http://associacioader.com/MAIL_DSTAR_O.php?plus=$dstar
+
+# PARA QUE MANDE UN MAIL SI SE TIENE PUESTO EL INDICATIVO EN EL dmrplus, brandmeister, solofusion o solodstar
 bm=`sed -n '2p'  ~/MMDVMHost/MMDVMBM.ini`
 plus=`sed -n '2p'  ~/MMDVMHost/MMDVMPLUS.ini`
 dstar=`sed -n '2p'  ~/MMDVMHost/MMDVMDSTAR.ini`
 fusion=`sed -n '2p'  ~/MMDVMHost/MMDVMFUSION.ini`
 
 
-sudo wget -post-data http://associacioader.com/MAIL_DMRPLUS_O.php?plus=$plus
-sudo wget -post-data http://associacioader.com/MAIL_BRANDMEISTER_O.php?plus=$bm
-sudo wget -post-data http://associacioader.com/MAIL_FUSION_O.php?plus=$fusion
-sudo wget -post-data http://associacioader.com/MAIL_DSTAR_O.php?plus=$dstar
+
+sudo wget -post-data http://associacioader.com/MAIL_ORANGEPI.php?plus=$plus'<DMR+   '$bm'<BRANDMEISTER   '$dstar'<SOLODSTAR   '$fusion'<SOLOFUSION'
+
