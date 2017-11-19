69941c4bc988a8ce05cbac8654a2f60d0ee9ddc8
ea3eiz <ea3eiz@ea3eiz.com>
2017-11-20 00:00:44 +0100

    1

--- menu_extra_05.sh
+++ menu_extra_05.sh
@@ -223,6 +223,7 @@ clear
 						[sS]* ) echo ""
 						echo ">>>>>>>>> coloca dcs_default.cfg en /home/orangepi/.config/Microsoft/dvrptr/1.0.0.0 >>>>>"
 			            cd ~/SCRIPTS_ORANGE/
+                        sudo chmod 777 /home/orangepi/.config/Microsoft/dvrptr/1.0.0.0/dcs_default.cfg
 			            sudo rm /home/orangepi/.config/Microsoft/dvrptr/1.0.0.0/dcs_default.cfg
                         sudo cp -f dcs_default.cfg /home/orangepi/.config/Microsoft/dvrptr/1.0.0.0/
                         sudo chmod 640 /home/orangepi/.config/Microsoft/dvrptr/1.0.0.0/dcs_default.cfg
