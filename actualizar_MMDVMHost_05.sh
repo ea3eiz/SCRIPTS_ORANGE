4a6305aea566536542ce08d15695ace44f5dafcc
ea3eiz <ea3eiz@ea3eiz.com>
2017-11-27 11:07:52 +0100

    1

--- actualizar_MMDVMHost_05.sh
+++ actualizar_MMDVMHost_05.sh
@@ -37,7 +37,7 @@ clear
                     mkdir ~/.copias
                     sudo rm ~/Downloads/MMDVM*.*
                     cd ~/
-                    sudo mv -r MMDVMHost MMDVMHost_old
+                    sudo mv MMDVMHost MMDVMHost_old
                     git clone https://github.com/g4klx/MMDVMHost
                     cd ~/MMDVMHost
                     make
