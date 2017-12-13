91a4417376f9dfa0907c0052112d2cdd84bb1f60
ea3eiz <ea3eiz@ea3eiz.com>
2017-12-13 22:29:19 +0100

    1

--- actualizar_MMDVMHost_05.sh
+++ actualizar_MMDVMHost_05.sh
@@ -37,6 +37,7 @@ clear
                     mkdir ~/.copias
                     sudo rm -R ~/Downloads/MMDVM*.*
                     cd ~/
+                    sudo chamod 77 -R MMDVMHost_anterior
                     sudo mv -f -R MMDVMHost MMDVMHost_anterior
                     #sudo rm -R MMDVMHost_old
                     #sudo mv -f MMDVMHost MMDVMHost_old
