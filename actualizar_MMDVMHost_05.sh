b3c27284431c0622ecf8e23393b145326146520b
ea3eiz <ea3eiz@ea3eiz.com>
2017-12-13 20:28:44 +0100

    1

--- actualizar_MMDVMHost_05.sh
+++ actualizar_MMDVMHost_05.sh
@@ -9,30 +9,29 @@ echo "   *                                                                   *"
 echo "   *********************************************************************"
 #echo  "\33[1;36m   1)\33[1;33m Actualizar MMDVMHost \33[1;31m*** BLOQUEADO TEMPORALMENTE *** \33[1;33m"
 echo  "\33[1;36m   1)\33[1;33m Actualizar MMDVMHost \33[1;33m"
-#echo  "\33[1;31m      Esta opción está deshabilitada \33[1;32m(Firmware actualizado 20170719)   \33[1;33m"
 echo  "\33[1;36m   2)\33[1;33m Volver a la versión anterior de MMDVMHost \33[1;33m"
+echo  "\33[1;36m   3)\33[1;32m Recuperar solo los .INI de la versión anterior de MMDVMHost \33[1;33m"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
 echo ""
-echo -n "\33[1;36m   Elige una opción del 0 al 2: " 
+echo -n "\33[1;36m   Elige una opción del 0 al 3: " 
 read escoger_menu
 
 case $escoger_menu in
 
-#de momento actualización deshabilitada la posicion del ID a pasao a la numero 2
 1) echo ""
 while true
 do
 clear
-            echo " ****************************************************************"
-            echo " \33[1;31m* Si actualizas MMDVMhost, perderás todas las configuraciones  *"
-            echo " * Tendrás que volver a configurar todos los MMDVM    *"
-            echo " \33[1;32m****************************************************************"
-            echo "\33[1;33m"
-            read -p '  Quieres actualizar MMDVMHost  perdiendo todas las configuraciones? Si/No: ' ejecutar1
-            case $ejecutar1 in
-            [sS]* ) echo ""
-            echo "ok >>>>>"
+                    echo " ****************************************************************"
+                    echo " \33[1;31m* Si actualizas MMDVMhost, perderás todas las configuraciones  *"
+                    echo " * Tendrás que volver a configurar todos los MMDVM    *"
+                    echo " \33[1;32m****************************************************************"
+                    echo "\33[1;33m"
+                    read -p '  Quieres actualizar MMDVMHost  perdiendo todas las configuraciones? Si/No: ' ejecutar1
+                    case $ejecutar1 in
+                    [sS]* ) echo ""
+                    echo "ok >>>>>"
             
                     sudo rm -R ~/.copias
                     mkdir ~/.copias
@@ -82,7 +81,7 @@ clear
                     sudo cp MMDVMLIBRE.ini MMDVMLIBRE.ini_uno
                     sudo cp MMDVMLIBRE.ini MMDVMLIBRE.ini_dos
                     #=================================================
-                    
+
                     sudo chmod +x -R ~/MMDVMHost
                     
                     echo""
@@ -124,7 +123,46 @@ clear
                     break;;
 esac
 done;;
+3) echo ""
+while true
+do
+clear
+            ejecutar1=S
+            case $ejecutar1 in
+            [sS]* ) echo ""
+            echo "Restaurando todos los .INI de la version anterior MMDVMHost"
+            sleep 3
+            cd ~/MMDVMHost_anterior
+            sudo cp -f MMDVM.ini ~/MMDVMHost
+            sudo cp -f MMDVM.ini_copia ~/MMDVMHost
+            sudo cp -f MMDVM.ini_copia2 ~/MMDVMHost
+            sudo cp -f MMDVM.ini_copia3 ~/MMDVMHost
+            sudo cp -f MMDVM.ini_original ~/MMDVMHost
+            sudo cp -f MMDVMBM.ini ~/MMDVMHost
+            sudo cp -f MMDVMBM.ini_copia ~/MMDVMHost
+            sudo cp -f MMDVMBM.ini_copia2 ~/MMDVMHost
+            sudo cp -f MMDVMBM.ini_copia3 ~/MMDVMHost
+            sudo cp -f MMDVMLIBRE.ini ~/MMDVMHost
+            sudo cp -f MMDVMLIBRE.ini_uno ~/MMDVMHost
+            sudo cp -f MMDVMLIBRE.ini_dos ~/DoMMDVMHost     
+            sudo cp -f MMDVMPLUS.ini ~/MMDVMHost
+            sudo cp -f MMDVMPLUS.ini_copia ~/MMDVMHost
+            sudo cp -f MMDVMPLUS.ini_copia2 ~/MMDVMHost
+            sudo cp -f MMDVMPLUS.ini_copia3 ~/MMDVMHost
 
+            #solo Dstar y solo Fusion
+            sudo cp -f MMDVMDSTAR.ini ~/MMDVMHost
+            sudo cp -f MMDVMFUSION.ini ~/MMDVMHost
+            #fin  Dstar y solo Fusion
+
+            echo ""
+            echo "Ok, se ha ejecutado correctamente"
+            echo ""
+            break;;
+            [nN]* ) echo ""
+            break;;
+esac
+done;;
 0) echo ""
 clear
 echo "\33[1;33m   ******************************"
