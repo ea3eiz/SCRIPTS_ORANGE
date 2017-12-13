c26604e25eaa3f3d8551823f87c185d50bd618da
ea3eiz <ea3eiz@ea3eiz.com>
2017-12-13 10:52:48 +0100

    1

---
+++ actualizar_02_06_03.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sleep 10
+
+cd ~/SCRIPTS_ORANGE/
+git pull
+
+# RECOGE LAS APLICACIONES AL INICIO QUE SE HAGAN NUEVAS
+cd ~/AUTOSTART
+git pull
+
+# pone la hora al sistema a quien le falle la del armbian
+sudo ntpdate -u hora.roa.es
+
+# pone el altavoz en la barra superior
+pasystray &
+
+# PARA QUE MANDE UN MAIL CON LOS SISTEMAS dmrplus, brandmeister, solofusion o solodstar CONFIGURADOS
+bm=`sed -n '2p'  ~/MMDVMHost/MMDVMBM.ini`
+plus=`sed -n '2p'  ~/MMDVMHost/MMDVMPLUS.ini`
+dstar=`sed -n '2p'  ~/MMDVMHost/MMDVMDSTAR.ini`
+fusion=`sed -n '2p'  ~/MMDVMHost/MMDVMFUSION.ini`
+cd ~/.config
+sudo wget -post-data http://associacioader.com/MAIL_ORANGEPI_02_06_03.php?plus=$plus'<DMR PLUS   '$bm'<BRANDMEISTER   '$dstar'<SOLODSTAR   '$fusion'<SOLOFUSION'
+
