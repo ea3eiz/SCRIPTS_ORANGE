c6d9e1792387c8bcf0a84ffebe36c76cdad78ade
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-19 22:08:07 +0100

    1

--- Actualizar_BlueDV_05.sh
+++ Actualizar_BlueDV_05.sh
@@ -8,6 +8,8 @@ echo "   *      Script para hacer actualizaciones y varios \33[1;33mV.02.05     
 echo "   *                          \33[1;31mby EA3EIZ\33[1;32m                               *"
 echo "   ********************************************************************"
 echo "\33[1;36m   1)\33[1;37m Actualizar BlueDV"
+echo "\33[1;36m   2)\33[1;37m Volver a la versión anterior del BlueDV"
+
 #echo "\33[1;36m   2)\33[1;37m Actualizar DV4mini Control Panel (Stick ID:8D-6E-8C V1,77)"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
@@ -65,6 +67,136 @@ echo "\33[1;33m"
 			break;;
 esac
 done;;
+
+
+10) echo ""
+while true
+do
+clear
+echo "\33[1;33m"
+	        read -p ' Quieres actualizar el BlueDV Si/No: ' ejecutar1
+		    case $ejecutar1 in
+			[sS]* ) echo ""
+			echo "ok >>>>>"
+if [ -d ~/bluedv_anterior/ ];
+then
+			clear
+			echo "\33[1;32mYA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE"
+			sleep 1
+			clear
+			echo "\33[1;31mYA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE"
+			sleep 1
+			clear
+			echo "\33[1;32mYA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE"
+			sleep 1
+			clear
+			echo "\33[1;31mYA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE"
+			sleep 1
+			clear
+			echo "\33[1;32m******************************************************"
+			echo "\33[1;32m* YA TIENES ACTUALIZADA LA ÚLTIMA VERSIÓN DISPONIBLE *"
+			echo "\33[1;32m******************************************************"
+			sleep 5
+else
+			cd ~/
+  			mkdir bluedv_anterior
+            sudo cp bluedv/*.* bluedv_anterior/
+            sudo rm -r ~/bluedv
+			git clone https://github.com/ea3eiz/bluedv
+			
+			cd ~/bluedv
+			sudo chmod 777 DExtra_Hosts.txt
+		
+fi	
+			echo "\33[1;32m******************************************************"
+			echo "\33[1;32m*  SE HA ACTUALIZADO A LA ÚLTIMA VERSIÓN DISPONIBLE  *"
+			echo "\33[1;32m******************************************************"
+			sleep 3
+exit;
+		    echo ""
+			echo "Ok, se ha ejecutado correctamente"
+			echo ""
+			break;;
+			[nN]* ) echo ""
+			break;;
+esac
+done;;
+20) echo ""
+while true
+do
+clear
+echo "\33[1;33m"
+	        read -p ' Quieres volver a la versión anterior Si/No: ' ejecutar1
+		    case $ejecutar1 in
+			[sS]* ) echo ""
+			echo "ok >>>>>"
+if [ -d ~/bluedv_anterior/ ];
+then
+			clear
+			echo "\33[1;32mVOLVIENDO A LA VERSIÓN ANTERIOR"
+			sleep 1
+			clear
+			echo "\33[1;31mVOLVIENDO A LA VERSIÓN ANTERIOR"
+			sleep 1
+			clear
+			echo "\33[1;32mVOLVIENDO A LA VERSIÓN ANTERIOR"
+			sleep 1
+			clear
+			echo "\33[1;31mVOLVIENDO A LA VERSIÓN ANTERIOR"
+			sleep 1
+			clear
+			echo "\33[1;32m***********************************"
+			echo "\33[1;32m* VOLVIENDO A LA VERSIÓN ANTERIOR *"
+			echo "\33[1;32m***********************************"
+			sleep 5
+			cd ~/
+            sudo cp bluedv_anterior/*.* bluedv/
+            sudo rm -R bluedv_anterior
+			
+else
+			
+
+
+clear
+			echo "\33[1;32mYA ESTÁS EN LA VERSIÓN ANTERIOR, NO HAS ACTUALIZADO NUNCA"
+			sleep 1
+			clear
+			echo "\33[1;31mYA ESTÁS EN LA VERSIÓN ANTERIOR, NO HAS ACTUALIZADO NUNCA"
+			sleep 1
+			clear
+			echo "\33[1;32mYA ESTÁS EN LA VERSIÓN ANTERIOR, NO HAS ACTUALIZADO NUNCA"
+			sleep 1
+			clear
+			echo "\33[1;31mYA ESTÁS EN LA VERSIÓN ANTERIOR, NO HAS ACTUALIZADO NUNCA"
+			sleep 1
+			clear
+			echo "\33[1;32m*************************************************************"
+			echo "\33[1;32m* YA ESTÁS EN LA VERSIÓN ANTERIOR, NO HAS ACTUALIZADO NUNCA *"
+			echo "\33[1;32m*************************************************************"
+			sleep 5
+
+
+sleep 5
+
+		
+fi	
+exit;
+		    echo ""
+			echo "Ok, se ha ejecutado correctamente"
+			echo ""
+			break;;
+			[nN]* ) echo ""
+			break;;
+esac
+done;;
+
+
+
+
+
+
+
+
 0) echo ""
 clear
 echo "\33[1;33m   ******************************"
