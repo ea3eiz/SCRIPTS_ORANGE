e10994f0a6a0b5194e265d8723015c7bd888f0f2
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-20 16:20:11 +0100

    1

--- Actualizar_BlueDV_05.sh
+++ Actualizar_BlueDV_05.sh
@@ -9,8 +9,7 @@ echo "   *                          \33[1;31mby EA3EIZ\33[1;32m                 
 echo "   ********************************************************************"
 echo "\33[1;36m   1)\33[1;37m Actualizar BlueDV"
 echo "\33[1;36m   2)\33[1;37m Volver a la versión anterior del BlueDV"
-
-HOY=$(date)
+HOY=$(date +%Y%m%d)
 echo "$HOY"
 #echo "\33[1;36m   2)\33[1;37m Actualizar DV4mini Control Panel (Stick ID:8D-6E-8C V1,77)"
 echo ""
