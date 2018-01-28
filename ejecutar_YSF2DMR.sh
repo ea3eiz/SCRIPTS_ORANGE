50d04567ce99724a0f49483fe124148290f1c2a7
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-28 13:27:38 +0100

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "6c Exec=sudo sh cerrar_YSF2DMR.sh" ~/Desktop/Abrir_YSF2DMR.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF2DMR_ON.png" ~/Desktop/Abrir_YSF2DMR.desktop
-sed -i "11c Name[es_ES]=Cerrar YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
+#sed -i "6c Exec=sudo sh cerrar_YSF2DMR.sh" ~/Desktop/Abrir_YSF2DMR.desktop
+#sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF2DMR_ON.png" ~/Desktop/Abrir_YSF2DMR.desktop
+#sed -i "11c Name[es_ES]=Cerrar YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
 
 
 cd ~/YSF2DMR
