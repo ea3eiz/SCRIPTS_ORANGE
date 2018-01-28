3482cb6f2bdb3be46e2ef01298ed13cd513bf991
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-28 12:44:32 +0100

    1

--- cerrar_YSF2DMR.sh
+++ cerrar_YSF2DMR.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "6c Exec=sh -c 'cd /home/pi/V30/;lxterminal --geometry=80x20 -e sudo sh ejecutar_YSF2DMR.sh'" /home/pi/Desktop/AbrirsoloYSF.desktop
-sed -i "7c Icon=/home/pi/V30/YSF2DMR.png" /home/pi/Desktop/AbrirsoloYSF.desktop
-sed -i "11c Name[es_ES]=Abrir YSF2DMR" /home/pi/Desktop/AbrirsoloYSF.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/V30/;lxterminal --geometry=80x20 -e sudo sh ejecutar_YSF2DMR.sh'" ~/Desktop/Abrir_YSF2DMR.desktop
+sed -i "7c Icon=/home/pi/V30/YSF2DMR.png" ~/Desktop/Abrir_YSF2DMR.desktop
+sed -i "11c Name[es_ES]=Abrir YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
 
 sudo killall MMDVMFUSION
 sudo killall YSF2DMR
