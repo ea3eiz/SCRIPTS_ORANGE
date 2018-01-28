5933a2e5a91064fec3a8bb2623e07bbf11ddeecc
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-28 12:40:56 +0100

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "6c Exec=sudo sh cerrar_solofusion.sh" /home/pi/Desktop/AbrirsoloYSF.desktop
-sed -i "7c Icon=/home/pi/V30/YSF2DMR_ON.png" /home/pi/Desktop/AbrirsoloYSF.desktop
-sed -i "11c Name[es_ES]=Cerrar YSF2DMR" /home/pi/Desktop/AbrirsoloYSF.desktop
+sed -i "6c Exec=sudo sh cerrar_YSF2DMR.sh" ~/Desktop/AbrirsoloYSF.desktop
+sed -i "7c Icon=/home/pi/V30/YSF2DMR_ON.png" ~/Desktop/AbrirsoloYSF.desktop
+sed -i "11c Name[es_ES]=Cerrar YSF2DMR" ~/Desktop/AbrirsoloYSF.desktop
 
 
 cd ~/YSF2DMR
