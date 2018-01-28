0ced677221a2cd83e1415641a495cc596f994f70
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-28 12:33:54 +0100

    1

--- cerrar_YSF2DMR.sh
+++ cerrar_YSF2DMR.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "6c Exec=sh -c 'cd /home/pi/V30/;lxterminal --geometry=80x20 -e sudo sh ejecutar_solofusion.sh'" /home/pi/Desktop/AbrirsoloYSF.desktop
-sed -i "7c Icon=/home/pi/V30/SOLO_YSF.png" /home/pi/Desktop/AbrirsoloYSF.desktop
-sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Desktop/AbrirsoloYSF.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/V30/;lxterminal --geometry=80x20 -e sudo sh ejecutar_YSF2DMR.sh'" /home/pi/Desktop/AbrirsoloYSF.desktop
+sed -i "7c Icon=/home/pi/V30/YSF2DMR.png" /home/pi/Desktop/AbrirsoloYSF.desktop
+sed -i "11c Name[es_ES]=Abrir YSF2DMR" /home/pi/Desktop/AbrirsoloYSF.desktop
 
 sudo killall MMDVMFUSION
-sudo killall YSFGateway
+sudo killall YSF2DMR
