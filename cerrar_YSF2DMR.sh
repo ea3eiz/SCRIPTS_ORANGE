ed2920ba81a9e41e6eecce61c8d51d026108ee65
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-28 12:59:16 +0100

    1

--- cerrar_YSF2DMR.sh
+++ cerrar_YSF2DMR.sh
@@ -2,6 +2,5 @@
 sed -i "6c Exec=sudo sh ejecutar_YSF2DMR.sh" ~/Desktop/Abrir_YSF2DMR.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF2DMR.png" ~/Desktop/Abrir_YSF2DMR.desktop
 sed -i "11c Name[es_ES]=Abrir YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
-
 sudo killall MMDVMFUSION
 sudo killall YSF2DMR
