6f526b298a97186d3d232e0dc8593b5bf4fe38d7
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-29 09:35:15 +0100

    1

--- cerrar_YSF2DMR.sh
+++ cerrar_YSF2DMR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "6c Exec=sudo sh ejecutar_YSF2DMR.sh" ~/Desktop/Abrir_YSF2DMR.desktop
+sed -i "6c Exec=mate-terminal -x sh ejecutar_YSF2DMR.sh" ~/Desktop/Abrir_YSF2DMR.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF2DMR.png" ~/Desktop/Abrir_YSF2DMR.desktop
 sed -i "11c Name[es_ES]=Abrir YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
 sudo killall MMDVMFUSION
--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -8,5 +8,5 @@ sed -i "11c Name[es_ES]=Cerrar solo FUSION" ~/Desktop/AbrirsoloYSF.desktop
 cd ~/YSFClients/YSFGateway
 sudo mate-terminal -x ./YSFGateway YSFGateway.ini & cd ~/MMDVMHost
 sudo mate-terminal -x ./MMDVMFUSION MMDVMFUSION.ini
-
+#
 
