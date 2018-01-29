f3d73837ee77168a6b614350192cf6c00f393d32
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-29 09:26:45 +0100

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -5,10 +5,10 @@ sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF2DMR_ON.png" ~/Desktop/Abrir_YS
 sed -i "11c Name[es_ES]=Cerrar YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
 
 
-cd ~/YSF2DMR
+#cd ~/YSF2DMR
 
 
-sudo mate-terminal -x ./YSF2DMR YSF2DMR.in & cd ~/MMDVMHost
-sudo mate-terminal -x ./MMDVMFUSION MMDVMFUSION.ini
+#sudo mate-terminal -x ./YSF2DMR YSF2DMR.in & cd ~/MMDVMHost
+#sudo mate-terminal -x ./MMDVMFUSION MMDVMFUSION.ini
 #
 
