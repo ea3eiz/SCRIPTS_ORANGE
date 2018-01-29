a227c723f2c44bd26e778db5c3d89f7dca904f7e
ea3eiz <ea3eiz@ea3eiz.com>
2018-01-29 09:37:39 +0100

    1

--- cerrar_YSF2DMR.sh
+++ cerrar_YSF2DMR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i "6c Exec=mate-terminal -x sh ejecutar_YSF2DMR.sh" ~/Desktop/Abrir_YSF2DMR.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF2DMR.png" ~/Desktop/Abrir_YSF2DMR.desktop
-sed -i "11c Name[es_ES]=Abrir YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
+sed -i "10c Name[es_ES]=Abrir YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
 sudo killall MMDVMFUSION
 sudo killall YSF2DMR
--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -2,7 +2,7 @@
 
 sed -i "6c Exec=mate-terminal -x sh cerrar_YSF2DMR.sh" ~/Desktop/Abrir_YSF2DMR.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF2DMR_ON.png" ~/Desktop/Abrir_YSF2DMR.desktop
-sed -i "11c Name[es_ES]=Cerrar YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
+sed -i "10c Name[es_ES]=Cerrar YSF2DMR" ~/Desktop/Abrir_YSF2DMR.desktop
 
 
 #cd ~/YSF2DMR
