13e8ef72b944ae53d82d32b2ba6330a32c7a2665
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-03 11:27:43 +0100

    1

--- menu_autoarranque_05.sh
+++ menu_autoarranque_05.sh
@@ -311,7 +311,7 @@ solof=`grep "DVRPTR" ~/autoarranque.ini`
 #==================================================================================
 
 #==================================================================================
-echo -n "\33[1;36m  26)\33[1;33m  Poner YSF2DMR en el autoarranque       - "
+echo -n "\33[1;36m  26)\33[1;33m  Poner YSF2DMR en el autoarranque      - "
 ysf_dmr=`grep "F2DMR" ~/autoarranque.ini`
 ysf_dmr=`expr substr $ysf_dmr 7 3`
 if [ $ysf_dmr = "OFF" ]
