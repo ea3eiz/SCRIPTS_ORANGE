cf4ef001a31ca3bf0809d6e488359a40d35fd9f3
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-04 01:06:00 +0100

    1

--- nota_version_06.sh
+++ nota_version_06.sh
@@ -45,9 +45,9 @@ echo "\33[1;31m   Prohibido modificar total o parcialmente el contenido de esta 
 echo "\33[1;37m"
 						echo -n "\33[1;32m   Info del sistema: \33[1;37m"
 						cat /proc/version
-						echo -n "Temperatura de la CPU: "
+						echo -n "\33[1;32m   Temperatura de la CPU: \33[1;37m"
 						cat /sys/class/thermal/thermal_zone0/temp;
-						echo "Grados"
+						echo "                           Grados"
                         echo -n "\33[1;36m   Pulsa la tecla ENTER para salir"
                         read A
 						exit;
