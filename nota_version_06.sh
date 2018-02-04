d233bcc9c20b3bad1636e45da5f7a9d3326d51c9
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-04 01:20:47 +0100

    1

--- nota_version_06.sh
+++ nota_version_06.sh
@@ -46,10 +46,8 @@ echo "\33[1;37m"
 						
 						echo -n "\33[1;32m   Temperatura de la CPU: \33[1;37m"
 						cat /sys/class/thermal/thermal_zone0/temp;
-						echo "S1"
-						echo "$0"
 						echo -n "\33[1;32m   Info del sistema: \33[1;37m"
-						cat /proc/version
+						mate-terminal -x cat /proc/version
 						
 						echo ""
                         echo -n "\33[1;36m   Pulsa la tecla ENTER para salir"
