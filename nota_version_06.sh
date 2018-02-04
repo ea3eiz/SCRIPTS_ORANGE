8ed2196f7e845fd54ff58199555eb89110380148
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-04 01:30:28 +0100

    1

--- nota_version_06.sh
+++ nota_version_06.sh
@@ -48,7 +48,7 @@ echo "\33[1;37m"
 						cat /sys/class/thermal/thermal_zone0/temp;
 						echo -n "\33[1;32m   Info del sistema: \33[1;37m"
 						cat /proc/version
-						
+						echo "$4"
 						echo ""
                         echo -n "\33[1;36m   Pulsa la tecla ENTER para salir"
                         read A
