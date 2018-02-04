2e579cb3a48d4e4486e6599c446343d9ebb9e990
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-04 21:42:36 +0100

    1

--- nota_version_06.sh
+++ nota_version_06.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# script para instalar md380tools
 
 echo "\33[1;32m   ************************************************************************************"
 echo "   *                                                                                  *"
@@ -10,45 +9,35 @@ echo ""
 echo "\33[1;36m   Gracias por descargar la imagen REM V.02.06"
 echo""
 echo "\33[1;32m   Imagen multi plataforma para \33[1;33mRADIO AFICIONADOS."
-# echo "   por el programa BlueDV en sus multi-sistemas." 
- echo ""
- echo "\33[1;37m   Tener presente las siguientes consideraciones:"
- echo""
- echo "\33[1;33m   - Utilizar adecuadamente los puertos de los correspondientes Módems."
-# echo ""
- echo "\33[1;33m   - Para salir de cualquier editor es necesario cerrar por el item 0)."
-# echo "\33[1;37m"
-# echo "   1º Al abrir por cualquiera de los iconos de MMDVMBM, DMR+, YSF ó ircDDB:"
-# echo "   Se cerrará la aplicación BlueDV."
-# echo ""
-# echo "   2º Al iniciar BlueDV, si estuvieran abiertos MMDVMBM, DMR+, YSF ó ircDDB"
-# echo "   estas se cerrarán automáticamente." 
-# echo ""
-# echo "   3º Al abrir ircDDB, si estuviera abierto BlueDV, este se cerrará automáticamente" 
-# echo "   sin presentar ningún mensaje que lo indique."
-# echo ""
-# echo "   4º Al abrir BlueDV, si estuvieran abiertos ircDDB y/o YSF se cerrarán automáticamente"
-# echo "   sin presentar ningún mensaje que lo indique." 
-# echo ""
-# echo "   5º Para salir de cualquier aplicación es necesario cerrar por el botón STOP"
-# echo "   correspondiente."
-# echo " "
-# echo "   6º Si abrimos primero el DV4mini, y después el solo D-Star Repeater + ircDDB"
-# echo "   NO se conecta D-STAR a ningún lado."
-# echo "   Pero si en primer lugar Abrimos D-Star Repeater + ircDDB y posteriormente el DV4mini"
-# echo "   todas las conexiones son correctas."
-# echo ""
+echo ""
+echo "\33[1;37m   Tener presente las siguientes consideraciones:"
+echo""
+echo "\33[1;33m   - Utilizar adecuadamente los puertos de los correspondientes Módems."
+echo "\33[1;33m   - Para salir de cualquier editor es necesario cerrar por el item 0)."
 echo "\33[1;32m"
 echo "   Imagen Linux basada en Armbian, creada por: EA3EIZ & EA4AOJ."
 echo ""
 echo "\33[1;31m   Prohibido modificar total o parcialmente el contenido de esta imagen."
-echo "\33[1;37m"
+echo " "					
 						
 						echo -n "\33[1;32m   Temperatura de la CPU: \33[1;37m"
-						cat /sys/class/thermal/thermal_zone0/temp;
-						echo -n "\33[1;32m   Info del sistema: \33[1;37m"
-						cat /proc/version
+						tem=$(cat /sys/class/thermal/thermal_zone0/temp)
+						tem=`expr substr $tem 1 2`
+						echo -n "$tem Grados"
+
+						var2=$(cat /proc/version)
+						var2=`echo "$var2" | tr -d '[[:space:]]'`
 						echo ""
+						var1=`expr substr $var2 1 22`
+						var3=`expr substr $var2 23 20`
+						var4=`expr substr $var2 43 62`
+						var5=`expr substr $var2 105 31`
+						echo -n "\33[1;32m   Info del sistema: \33[1;37m"
+						echo "$var1 $var3"
+						echo "   $var4"
+						echo "   $var5"
+						echo ""
+						
                         echo -n "\33[1;36m   Pulsa la tecla ENTER para salir"
                         read A
 						exit;
