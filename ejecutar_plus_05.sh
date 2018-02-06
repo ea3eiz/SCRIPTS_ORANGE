0c66157898b1e6c54237a998f5f079f92d63bcca
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-06 09:34:28 +0100

    1

--- ejecutar_plus_05.sh
+++ ejecutar_plus_05.sh
@@ -3,6 +3,7 @@ sed -i "6c Exec=mate-terminal -x sh cerrar_plus_05.sh" ~/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMRPLUS_ON.png" ~/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Cerrar DMR+" ~/Desktop/DMRPLUS.desktop
 
+sed -i "6c MMDVMPLUS=ON" ~/status.ini
 
 echo "\33[1;37m"
 cd ~/MMDVMHost
