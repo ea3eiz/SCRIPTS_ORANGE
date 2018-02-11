8bce36624f146fa1c596ce23239e5548fa87af73
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-11 10:28:46 +0100

    1

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -2,6 +2,8 @@
 puerto_router=$(awk "NR==11" /home/orangepi/status.ini) 
 puerto_modem=$(awk "NR==12" /home/orangepi/status.ini)
 cd ~/AMBEServer
+sudo killall AMBEserver
+sleep 5
 ./AMBEserver -p $puerto_router -i $puerto_modem
 
 				
