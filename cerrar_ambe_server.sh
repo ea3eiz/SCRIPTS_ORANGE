fee945597488f9b38d00cff7edddadca51605702
ea3eiz <ea3eiz@ea3eiz.com>
2018-02-12 01:13:36 +0100

    1

--- cerrar_ambe_server.sh
+++ cerrar_ambe_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "6cExec=mate-terminal -x ambeserver" ~/Desktop/Abrir_ambe_server.desktop
+sed -i "6cExec=mate-terminal -x sh ejecutar_ambe_server.sh" ~/Desktop/Abrir_ambe_server.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/AMBE_SERVER.png" ~/Desktop/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Abrir AMBE SERVER" ~/Desktop/Abrir_ambe_server.desktop
 
