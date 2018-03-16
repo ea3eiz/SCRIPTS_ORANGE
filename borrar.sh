e73de656f7b19bf3d2bb0fc50208948a6dab6670
ea3eiz <ea3eiz@ea3eiz.com>
2018-03-16 08:58:18 +0100

    1

--- borrar.sh
+++ borrar.sh
@@ -41,7 +41,7 @@ letra=c
 linea_sed_DS=$numero_linea$letra
 echo "$var1"
 
-echo -n "\33[1;36m   9)\33[0m DstPort               - \33[1;33m"
+echo -n "\33[1;36m   9)\33[0m LocalPort             - \33[1;33m"
 var=`grep -n -m 1 '\<LocalPort\>' ~/YSF2DMR/YSF2DMR.ini`
 var1=`grep -m 1 '\<LocalPort\>' ~/YSF2DMR/YSF2DMR.ini`
 buscar=":"
