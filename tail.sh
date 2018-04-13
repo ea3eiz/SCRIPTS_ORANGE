6cd938337a6d4a767c323d0558a89c6be3f2459e
ea3eiz <ea3eiz@ea3eiz.com>
2018-04-13 19:48:43 +0200

    1

---
+++ tail.sh
@@ -0,0 +1,27 @@
+
+#!bin/bash
+#prepara formato log ejp: MMDVM-2018-03-26.log
+hoy=$(date +%Y%m%d)
+ano=`expr substr $hoy 1 4`
+mes=`expr substr $hoy 5 2`
+dia=`expr substr $hoy 7 2`
+echo "$hoy"
+echo "$ano"
+echo "$mes"
+echo "$dia"
+fecha="MMDVM-"$ano"-"$mes"-"$dia".log"
+echo "$fecha"
+cd ~/MMDVMHost
+c=`tail -n 1 -c 2 $fecha`
+echo "$c"
+if [ "$c" = "%" ];
+then
+echo "esta el signo de %"
+sed -i "30c Call" ~/info_panel_control.ini
+else
+echo "$a"
+a=`tail -n 1 -c 15 $fecha`
+sed -i "30c $a" ~/info_panel_control.ini
+fi
+
+
---
+++ tgbm.sh
@@ -0,0 +1,19 @@
+
+#!bin/bash
+#prepara la presentación del indicativo que habla en Brandmeister
+hoy=$(date +%Y%m%d)
+ano=`expr substr $hoy 1 4`
+mes=`expr substr $hoy 5 2`
+dia=`expr substr $hoy 7 2`
+fecha="MMDVM-"$ano"-"$mes"-"$dia".log"
+cd ~/MMDVMHost
+c=`tail -n 1 -c 2 $fecha`
+if [ "$c" = "%" ];
+then
+echo "esta el signo de %"
+sed -i "1c ..........................................................................Call" ~/tgbm
+else
+tail -7 $fecha | tee ~/tgbm
+head -1 ~/tgbm | tee ~/tgbm
+tail -c 20 ~/tgbm
+fi
