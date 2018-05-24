f3d61e766036d1b667965fa12a7c5b3c5a264ba0
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 15:19:14 +0200

    1

--- cerrar_solodstar_04.sh
+++ cerrar_solodstar_04.sh
@@ -4,5 +4,7 @@ sed -i "6c Exec=mate-terminal -x sh ejecutar_solodstar_04.sh" ~/Desktop/Dstar_so
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_DSTAR.png" ~/Desktop/Dstar_solo.desktop
 sed -i "10c Name[es_ES]=Abrir D-STAR Repeater" ~/Desktop/Dstar_solo.desktop
 
+sed -i "9c dstarrepeater=OFF" ~/status.ini
+
 sudo killall ircddbgateway
 sudo killall dstarrepeater
--- ejecutar_solodstar_04.sh
+++ ejecutar_solodstar_04.sh
@@ -4,7 +4,7 @@ sed -i "6c Exec=mate-terminal -x sh cerrar_solodstar_04.sh" ~/Desktop/Dstar_solo
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_DSTAR_ON.png" ~/Desktop/Dstar_solo.desktop
 sed -i "10c Name[es_ES]=Cerrar D-STAR Repeater" ~/Desktop/Dstar_solo.desktop
 
-sed -i "13c SOLODSTAR=ON" ~/status.ini
+sed -i "9c dstarrepeater=ON" ~/status.ini
 
 ircddbgateway -gui  & dstarrepeater
 
