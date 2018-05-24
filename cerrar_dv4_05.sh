75f237943ea9af1cbfa04bd0d00e926f0b3308f9
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 15:37:59 +0200

    1

--- cerrar_dv4_05.sh
+++ cerrar_dv4_05.sh
@@ -3,6 +3,8 @@ sed -i "6c Exec=sh ejecutar_dv4_05.sh" ~/Desktop/DV4mini.desktop
 sed -i "7c Icon=/home/orangepi/dv4mini/dv4k.png" ~/Desktop/DV4mini.desktop
 sed -i "11c Name[es_ES]=Abrir DV4mini" ~/Desktop/DV4mini.desktop
 
+sed -i "4c D-DV4mini=OFF" ~/status.ini
+
 sudo su | ps aux | grep dv4mini.exe | grep -v grep | awk '{print $2}' | xargs kill
 sudo killall dv_serial
 sudo killall python
