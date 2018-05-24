f6f99ca95558f5313d33de69d99365cf2484ea46
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 15:11:10 +0200

    1

--- cerrar_solofusion.sh
+++ cerrar_solofusion.sh
@@ -4,5 +4,7 @@ sed -i "6c Exec=sudo sh ejecutar_solofusion.sh" ~/Desktop/AbrirsoloYSF.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_YSF.png" ~/Desktop/AbrirsoloYSF.desktop
 sed -i "11c Name[es_ES]=Abrir solo FUSION" ~/Desktop/AbrirsoloYSF.desktop
 
+sed -i "12c SOLOFUSION=OFF" ~/status.ini
+
 sudo killall MMDVMFUSION
 sudo killall YSFGateway
--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -4,6 +4,7 @@ sed -i "6c Exec=sudo sh cerrar_solofusion.sh" ~/Desktop/AbrirsoloYSF.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_YSF_ON.png" ~/Desktop/AbrirsoloYSF.desktop
 sed -i "11c Name[es_ES]=Cerrar solo FUSION" ~/Desktop/AbrirsoloYSF.desktop
 
+sed -i "12c SOLOFUSION=ON" ~/status.ini
 
 cd ~/YSFClients/YSFGateway
 sudo mate-terminal -x ./YSFGateway YSFGateway.ini & cd ~/MMDVMHost
