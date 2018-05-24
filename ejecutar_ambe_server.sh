b0bef09a4767c01a7e72ba2889be39ba37b23849
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 15:27:46 +0200

    1

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -3,6 +3,9 @@ sed -i "6cExec=mate-terminal -x sh cerrar_ambe_server.sh" ~/Desktop/Abrir_ambe_s
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/AMBER_SERVER_ON.png" ~/Desktop/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Cerrar AMBE SERVER" ~/Desktop/Abrir_ambe_server.desktop
 
+
+sed -i "11c AMBE_SERVER=ON" ~/status.ini
+
 puerto_router=$(awk "NR==11" /home/orangepi/status.ini) 
 puerto_modem=$(awk "NR==12" /home/orangepi/status.ini)
 cd ~/AMBEServer
