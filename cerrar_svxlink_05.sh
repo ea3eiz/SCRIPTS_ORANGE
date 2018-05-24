add9cad285d36e096b1b5c0908526023b45b58a2
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 15:21:35 +0200

    1

--- cerrar_svxlink_05.sh
+++ cerrar_svxlink_05.sh
@@ -3,8 +3,7 @@ sed -i "6c Exec=mate-terminal -x sh ejecutar_svxlink_05.sh" ~/Desktop/Abrir_SVXL
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/ECHOLINK.png" ~/Desktop/Abrir_SVXLink.desktop
 sed -i "4c Name[es_ES]=Abrir SVXLINK" ~/Desktop/Abrir_SVXLink.desktop
 
-
-
+sed -i "8c SVXLINK=OFF" ~/status.ini
 
 sudo killall svxlink
 
--- ejecutar_svxlink_05.sh
+++ ejecutar_svxlink_05.sh
@@ -4,6 +4,8 @@ sed -i "6c Exec=mate-terminal -x sh cerrar_svxlink_05.sh" ~/Desktop/Abrir_SVXLin
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SVXLINK_ON.png" ~/Desktop/Abrir_SVXLink.desktop
 sed -i "4c Name[es_ES]=Cerrar SVXLINK" ~/Desktop/Abrir_SVXLink.desktop
 
+sed -i "8c SVXLINK=ON" ~/status.ini
+
     #[Desktop Entry]
     #Version=1.0
     #Type=Application
