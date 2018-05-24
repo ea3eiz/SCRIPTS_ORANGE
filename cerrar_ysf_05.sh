a311a7f954753c2aef886962861820ab94add885
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 15:42:20 +0200

    1

--- cerrar_ysf_05.sh
+++ cerrar_ysf_05.sh
@@ -4,3 +4,5 @@ sudo killall YSFGateway
 sed -i "6c Exec=mate-terminal -x sh ejecutar_ysf_05.sh" ~/Desktop/YSFGateway.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/C4FM.png" ~/Desktop/YSFGateway.desktop
 sed -i "11c Name[es_ES]=Abrir YSF" ~/Desktop/YSFGateway.desktop
+
+sed -i "3c YSF=OFF" ~/status.ini
--- ejecutar_ysf_05.sh
+++ ejecutar_ysf_05.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
-
-
-
-#[Desktop Entry]
-#Version=1.0
-#Type=Application
-#Name=YSFGateway
-#Comment=
-#Exec=mate-terminal -x sh ejecutar_ysf_05.sh
-#Icon=/home/orangepi/SCRIPTS_ORANGE/C4FM.png
-#Path=/home/orangepi/SCRIPTS_ORANGE
-#Terminal=false
-#StartupNotify=false
-#Name[es_ES]=Abrir YSF
-
-
-
 sed -i "6c Exec=mate-terminal -x sh cerrar_ysf_05.sh" ~/Desktop/YSFGateway.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/YSF_ON.png" ~/Desktop/YSFGateway.desktop
 sed -i "11c Name[es_ES]=Cerrar YSF" ~/Desktop/YSFGateway.desktop
 
+sed -i "3c YSF=ON" ~/status.ini
 
 cd ~/YSFClients/YSFGateway
 echo "\33[1;32m"
