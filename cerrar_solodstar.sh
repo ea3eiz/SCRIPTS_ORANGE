9083cb94adc6add549938f7bc5a1922b6973b5b4
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 13:50:05 +0200

    1

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -4,5 +4,8 @@ sed -i "6c Exec=mate-terminal -x sh -c 'cd /home/orangepi/SCRIPTS_ORANGE/;sh eje
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR.png" ~/Desktop/AbrirsoloDstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
 
+
+sed -i "13c SOLODSTAR=OFF" ~/status.ini
+
 sudo killall MMDVMDSTAR
 sudo killall ircddbgateway
--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -4,6 +4,7 @@ sed -i "6c Exec=sudo sh cerrar_solodstar.sh" ~/Desktop/AbrirsoloDstar.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" ~/Desktop/AbrirsoloDstar.desktop
 sed -i "11c Name[es_ES]=Cerrar solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
 
+sed -i "13c SOLODSTAR=ON" ~/status.ini
 
 cd ~/MMDVMHost
 echo "\33[1;32m"
