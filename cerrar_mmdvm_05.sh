cd00d07bb9f3d6c15024802987238ca2da1b4261
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-24 14:03:12 +0200

    1

--- cerrar_mmdvm_05.sh
+++ cerrar_mmdvm_05.sh
@@ -4,4 +4,6 @@ sed -i "6 cExec=mate-terminal -x sh ejecutar_mmdvm_05.sh" ~/Desktop/MMDVM.deskto
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/MMDVM.png" ~/Desktop/MMDVM.desktop
 sed -i "4c Name[es_ES]=Abrir Radio" ~/Desktop/MMDVM.desktop
 
+sed -i 5c MMDVM=OFF" ~/status.ini
+
 sudo killall MMDVMHost
--- ejecutar_mmdvm_05.sh
+++ ejecutar_mmdvm_05.sh
@@ -3,6 +3,8 @@ sed -i "6 cExec=mate-terminal -x sh cerrar_mmdvm_05.sh" ~/Desktop/MMDVM.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/MMDVM_ON.png" ~/Desktop/MMDVM.desktop
 sed -i "4c Name[es_ES]=Cerrar Radio" ~/Desktop/MMDVM.desktop
 
+sed -i 5c MMDVM=ON" ~/status.ini
+
 cd ~/MMDVMHost
 
 echo "\33[1;32m"
