4fdd3ce0a9b57a76f524cda75a9cdce3da01711a
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-25 09:03:20 +0200

    1

--- cerrar_bm_05.sh
+++ cerrar_bm_05.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sed -i "7c MMDVMBM=OFF" ~/status.ini
 sudo killall MMDVMBM
 cd ~/SCRIPTS_ORANGE
 #sudo sh cerrar_ysf_30.sh
@@ -10,4 +10,3 @@ cd ~/SCRIPTS_ORANGE
 sed -i "6cExec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/BM.desktop
-sed -i "7c MMDVMBM=OFF" ~/status.ini
