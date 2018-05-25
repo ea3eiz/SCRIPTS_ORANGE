ff31811b4bea5a6dc5704fb70d14fdbc564b4250
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-25 10:30:28 +0200

    1

--- actualizar_02_07_04.sh
+++ actualizar_02_07_04.sh
@@ -2,7 +2,7 @@
 
 #Quita todos los iconos verdes que se quedan al cerrar la imagen
 
-sed -i "6c Exec=mate-terminal -x sh ejecutar_bm_05.sh'" ~/Desktop/BM.desktop
+sed -i "6c Exec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/BM.desktop
 
