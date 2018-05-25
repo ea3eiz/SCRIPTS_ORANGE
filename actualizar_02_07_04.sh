2f686b0aafc49a4c8d760745f579b3008cabac78
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-25 10:23:45 +0200

    1

--- actualizar_02_07_04.sh
+++ actualizar_02_07_04.sh
@@ -29,9 +29,9 @@ pasystray &
 
 #Quita todos los iconos verdes que se quedan al cerrar la imagen
 
-sed -i "6c Exec=mate-terminal -x sh ejecutar_bm_05.sh'" ~/Desktop/Abrir_MMDVMBM
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/Abrir_MMDVMBM
-sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/Abrir_MMDVMBM
+sed -i "6c Exec=mate-terminal -x sh ejecutar_bm_05.sh'" ~/Desktop/BM.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
+sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/BM.desktop
 
 sleep 10
 
