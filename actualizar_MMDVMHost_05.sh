6bd75c17ca53feed45cecf94d407a5738ab3d374
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-25 14:56:16 +0200

    1

--- actualizar_MMDVMHost_05.sh
+++ actualizar_MMDVMHost_05.sh
@@ -36,6 +36,60 @@ echo ""
 else
 mkdir MMDVMHost_anterior
 fi
+
+
+                
+                    sudo chmod 777 -R ~/Downloads
+
+
+
+
+            cd ~/MMDVMHost
+            sudo cp -f MMDVM.ini ~/Downloads
+            sudo cp -f MMDVM.ini_copia ~/Downloads
+            sudo cp -f MMDVM.ini_copia2 ~/Downloads
+            sudo cp -f MMDVM.ini_copia3 ~/Downloads
+            sudo cp -f MMDVM.ini_original ~/Downloads
+            sudo cp -f MMDVMBM.ini ~/Downloads
+            sudo cp -f MMDVMBM.ini_copia ~/Downloads
+            sudo cp -f MMDVMBM.ini_copia2 ~/Downloads
+            sudo cp -f MMDVMBM.ini_copia3 ~/Downloads
+            sudo cp -f MMDVMLIBRE.ini ~/Downloads
+            sudo cp -f MMDVMLIBRE.ini_uno ~/Downloads
+            sudo cp -f MMDVMLIBRE.ini_dos ~/Downloads       
+            sudo cp -f MMDVMPLUS.ini ~/Downloads
+            sudo cp -f MMDVMPLUS.ini_copia ~/Downloads
+            sudo cp -f MMDVMPLUS.ini_copia2 ~/Downloads
+            sudo cp -f MMDVMPLUS.ini_copia3 ~/Downloads
+
+            #solo Dstar y solo Fusion
+            sudo cp -f MMDVMDSTAR.ini ~/Downloads
+            sudo cp -f MMDVMFUSION.ini ~/Downloads
+            #fin  Dstar y solo Fusion
+
+            cd  ~/YSF2DMR
+            cp -f YSF2DMR.ini ~/Downloads
+            cp -f YSF2DMR.ini_copia_01 ~/Downloads
+            cp -f YSF2DMR.ini_copia_02 ~/Downloads
+
+            cd  ~/YSFClients/YSFGateway/
+            sudo cp -f YSFGateway.ini ~/Downloads
+            
+            cd ~/bluedv/
+            sudo cp -f BlueDVconfig.ini ~/Downloads
+
+            cd /usr/local/etc/svxlink/
+            sudo cp -f svxlink.conf ~/Downloads
+
+            cd /usr/local/etc/svxlink/svxlink.d/
+            sudo cp -f ModuleEchoLink.conf ~/Downloads
+
+
+
+
+
+
+
                     cp MMDVMHost/*.* MMDVMHost_anterior/
                     sudo rm -r MMDVMHost
                     git clone https://github.com/g4klx/MMDVMHost
