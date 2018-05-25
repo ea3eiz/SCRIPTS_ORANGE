8c611200b1828803f33c63897f40af098ec6c2f6
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-25 09:15:53 +0200

    1

--- ejecutar_bm_05.sh
+++ ejecutar_bm_05.sh
@@ -23,7 +23,7 @@
 #else
 
 
-sed -i "6cExec=mate-terminal -x sh cerrar_bm_05.sh" ~/Desktop/BM.desktop
+sed -i "6c Exec=mate-terminal -x sh cerrar_bm_05.sh" ~/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BRANDMEISTER_ON.png" ~/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Cerrar BM" ~/Desktop/BM.desktop
 
