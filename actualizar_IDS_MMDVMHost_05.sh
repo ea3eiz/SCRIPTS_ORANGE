a6e98967db5ae204185e60baac6c68911d500854
ea3eiz <ea3eiz@ea3eiz.com>
2018-05-31 20:03:57 +0200

    1

--- actualizar_IDS_MMDVMHost_05.sh
+++ actualizar_IDS_MMDVMHost_05.sh
@@ -11,4 +11,14 @@ letrac=c
 linea_sed=$numero_linea$letrac
 sed -i "$linea_sed DMRIDPATH=~/MMDVMHost" ~/MMDVMHost/linux/DMRIDUpdate.sh
 
+# DMR IDs now served by RadioID.net
+var=`grep -n -m 1 '\<DATABASEURL\>' ~/MMDVMHost/linux/DMRIDUpdate.sh`
+buscar=":"
+largo_linea=`expr index $var $buscar`
+largo_linea=`expr $largo_linea - 1`
+numero_linea=`expr substr $var 1 $largo_linea`
+letrac=c
+linea_sed=$numero_linea$letrac
+sed -i "$linea_sed DATABASEURL='https://ham-digital.org/status/users.csv'" ~/MMDVMHost/linux/DMRIDUpdate.sh
+
 sudo sh ~/MMDVMHost/linux/DMRIDUpdate.sh
