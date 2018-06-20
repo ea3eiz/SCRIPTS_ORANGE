bce3b20b020a95dc04f7d1488fc8ec249cd176d7
ea3eiz <ea3eiz@ea3eiz.com>
2018-06-21 00:07:07 +0200

    1

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -8,10 +8,12 @@ sed -i "10c Name[es_ES]=Cerrar DMR2NXDN" ~/Desktop/Abrir_DMR2NXDN.desktop
 #sed -i "14c NXDN2DMR=ON" ~/status.ini
 
 cd ~/DMR2NXDN
-
+sleep 1
 sudo mate-terminal -x ./DMR2NXDN DMR2NXDN.ini & 
+sleep 1
 cd ~/MMDVMHost
 sudo mate-terminal -x ./MMDVMHost MMDVMDMR2NXDN.ini &
+sleep 1
 cd ~/NXDNClients/NXDNGateway
 sudo mate-terminal -x ./NXDNGateway NXDNGateway.ini
 
