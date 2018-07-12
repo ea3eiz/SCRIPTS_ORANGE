67460da392bffe6f629bc42531e826b35b2cd4a4
ea3eiz <ea3eiz@ea3eiz.com>
2018-07-12 12:26:20 +0200

    1

--- ejecutar_bm_05.sh
+++ ejecutar_bm_05.sh
@@ -14,3 +14,11 @@ echo "***********************************************"
 echo "\33[38;5;138m"
 sudo ./MMDVMBM MMDVMBM.ini
 clear
+sudo killall MMDVMBM
+cd ~/SCRIPTS_ORANGE
+sed -i "7c MMDVMBM=OFF" ~/status.ini
+
+sed -i "6cExec=mate-terminal -x sh ejecutar_bm_05.sh" ~/Desktop/BM.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/DMR.png" ~/Desktop/BM.desktop
+sed -i "10c Name[es_ES]=Abrir BM" ~/Desktop/BM.desktop
+
