91d0f3e994368cd8d3ef14083f5b9cc616629067
ea3eiz <ea3eiz@ea3eiz.com>
2018-07-13 01:06:19 +0200

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -9,3 +9,8 @@ sed -i "13c SOLODSTAR=ON" ~/status.ini
 cd ~/MMDVMHost
 echo "\33[1;32m"
 sudo ./MMDVMDSTAR MMDVMDSTAR.ini & ircddbgateway -gui
+
+sed -i "6c Exec=mate-terminal -x sh -c 'cd /home/orangepi/SCRIPTS_ORANGE/;sh ejecutar_solodstar.sh'" ~/Desktop/AbrirsoloDstar.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR.png" ~/Desktop/AbrirsoloDstar.desktop
+sed -i "11c Name[es_ES]=Abrir solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
+sed -i "13c SOLODSTAR=OFF" ~/status.ini
