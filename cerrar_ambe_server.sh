5916b7889e9af4343a60d452252eacb2a5d663e4
ea3eiz <ea3eiz@ea3eiz.com>
2018-07-12 11:08:43 +0200

    1

--- cerrar_ambe_server.sh
+++ cerrar_ambe_server.sh
@@ -7,6 +7,8 @@ sed -i "11c AMBE_SERVER=OFF" ~/status.ini
 
 sudo killall AMBEserver
 
-				
+sed -i "6cExec=mate-terminal -x sh ejecutar_ambe_server.sh" ~/Desktop/Abrir_ambe_server.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/AMBE_SERVER.png" ~/Desktop/Abrir_ambe_server.desktop
+sed -i "10c Name[es_ES]=Abrir AMBE SERVER" ~/Desktop/Abrir_ambe_server.desktop				
 						
 						
