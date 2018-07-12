f021eb4b9a064bb607a134f541f8d89439024ffd
ea3eiz <ea3eiz@ea3eiz.com>
2018-07-12 12:22:24 +0200

    1

--- ejecutar_bluedv_05.sh
+++ ejecutar_bluedv_05.sh
@@ -8,3 +8,10 @@ sed -i "2c BlueDV=ON" ~/status.ini
 
 cd ~/bluedv
 sudo mono BlueDV.exe
+
+sed -i "6c Exec=sh ejecutar_bluedv_05.sh" ~/Desktop/BlueDV.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/BLUEDV.png" ~/Desktop/BlueDV.desktop
+sed -i "8c Path=/home/orangepi/SCRIPTS_ORANGE" ~/Desktop/BlueDV.desktop
+sed -i "11c Name[es_ES]=Abrir BlueDV" ~/Desktop/BlueDV.desktop
+
+sed -i "2c BlueDV=OFF" ~/status.ini
