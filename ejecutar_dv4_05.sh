329270abef935f56dcacdf7e6eb99f113c2585f4
ea3eiz <ea3eiz@ea3eiz.com>
2018-07-12 12:42:41 +0200

    1

--- ejecutar_dv4_05.sh
+++ ejecutar_dv4_05.sh
@@ -9,3 +9,8 @@ sed -i "4c DV4mini=ON" ~/status.ini
 cd ~/dv4mini
 mono dv4mini.exe
 
+sed -i "6c Exec=sh ejecutar_dv4_05.sh" ~/Desktop/DV4mini.desktop
+sed -i "7c Icon=/home/orangepi/dv4mini/dv4k.png" ~/Desktop/DV4mini.desktop
+sed -i "11c Name[es_ES]=Abrir DV4mini" ~/Desktop/DV4mini.desktop
+
+sed -i "4c DV4mini=OFF" ~/status.ini
