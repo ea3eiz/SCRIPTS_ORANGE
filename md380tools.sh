d7ab7360e1728552e28e789b80a563cd3d176444
ea3eiz <ea3eiz@ea3eiz.com>
2018-10-17 20:39:33 +0200

    1

--- md380tools.sh
+++ md380tools.sh
@@ -170,8 +170,8 @@ clear
                         cd ~/md380tools
                         sudo rm user.bin
                         echo "Introduciendo USER.BIN"
-                        # sudo wget http://42561.noip.us/user.bin
-                        sudo wget https://kg5rki.com/MD380_AIO/user.bin
+                        sudo wget http://42561.noip.us/user.bin
+                        #sudo wget https://kg5rki.com/MD380_AIO/user.bin
                         sudo ./md380-tool spiflashwrite user.bin 0x100000
 
                         break;;
