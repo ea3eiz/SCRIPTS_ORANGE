b9a4cf257464fdf942b63c4fe4567862939b4160
ea3eiz <ea3eiz@ea3eiz.com>
2019-01-21 10:58:15 +0100

    Update menu_extra_05.sh

--- menu_extra_05.sh
+++ menu_extra_05.sh
@@ -38,7 +38,7 @@ clear
 		        case $ejecutar1 in
 			[sS]* ) echo ""
 			echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
-                        cd /usr/local/share/opendv/
+                        #cd /usr/local/share/opendv/
                         #sudo curl --fail -o DExtra_Hosts.txt -s http://www.pistar.uk/downloads/DExtra_Hosts.txt
                         #sudo curl --fail -o DCS_Hosts.txt -s http://www.pistar.uk/downloads/DCS_Hosts.txt
                         #sudo curl --fail -o DPlus_Hosts.txt -s http://www.pistar.uk/downloads/DPlus_Hosts.txt
@@ -52,7 +52,6 @@ clear
 
                         sudo usermod -a -G dialout orangepi
                         sudo usermod -a -G uucp orangepi
-                      
 
                         exit;
                         break;;
