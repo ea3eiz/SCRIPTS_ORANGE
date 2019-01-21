55fe4f7f278784ead6ab662a9fc807d00b06c93f
ea3eiz <ea3eiz@ea3eiz.com>
2019-01-21 10:33:21 +0100

    1

--- menu_extra_05.sh
+++ menu_extra_05.sh
@@ -38,6 +38,10 @@ clear
 		        case $ejecutar1 in
 			[sS]* ) echo ""
 			echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
+                        cd /usr/local/share/opendv/
+                        sudo curl --fail -o DExtra_Hosts.txt -s http://www.pistar.uk/downloads/DExtra_Hosts.txt
+                        sudo curl --fail -o DCS_Hosts.txt -s http://www.pistar.uk/downloads/DCS_Hosts.txt
+                        sudo curl --fail -o DPlus_Hosts.txt -s http://www.pistar.uk/downloads/DPlus_Hosts.txt
                         git pull 
                         sleep 3
                         cd ~/
