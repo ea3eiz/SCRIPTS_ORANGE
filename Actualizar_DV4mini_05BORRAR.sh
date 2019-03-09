7da02a697f8a546c2d4d18e393243d3f3613e08f
ea3eiz <ea3eiz@ea3eiz.com>
2019-03-09 20:35:07 +0100

    Update Actualizar_DV4mini_05BORRAR.sh

--- Actualizar_DV4mini_05BORRAR.sh
+++ Actualizar_DV4mini_05BORRAR.sh
@@ -8,7 +8,7 @@ echo "   *           Script para Actualizar \33[1;33mDV4MINI20190317   \33[1;31m
 echo "   *                                                                   *"
 echo "   *********************************************************************"
 echo  "\33[1;36m   1)\33[1;33m Actualizar \33[1;32mDV4MINI20190307   \33[1;33m"
-echo  "\33[1;36m   2)\33[1;33m Volver a la versión \33[1;32mDV4MINI20190127   \33[1;33m"
+echo  "\33[1;36m   2)\33[1;33m Volver a la versión \33[1;32mDV4MINI20170517   \33[1;33m"
 echo ""
 echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
 echo ""
@@ -60,13 +60,11 @@ clear
                         [sS]* ) echo ""
                         echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
                         sudo rm -r /home/orangepi/SCRIPTS_ORANGE/DV4MINI/
-                        read a
                         cd /home/orangepi/SCRIPTS_ORANGE
                         git clone http://github.com/ea3eiz/DV4MINI
                         sudo rm -r /home/orangepi/dv4mini
                         read a
                         sudo mkdir /home/orangepi/dv4mini
-                        read a
                         cd /home/orangepi/SCRIPTS_ORANGE/DV4MINI/20170517
                         cp dv_serial /home/orangepi/dv4mini
                         cp dv4mini.exe /home/orangepi/dv4mini
