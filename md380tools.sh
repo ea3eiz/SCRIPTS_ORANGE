a5653f97a075c2c4b334fb81bf3b35cf43ee5b9d
ea3eiz <ea3eiz@ea3eiz.com>
2019-04-17 18:15:29 +0200

    Update md380tools.sh

--- md380tools.sh
+++ md380tools.sh
@@ -170,9 +170,9 @@ clear
                         cd ~/md380tools
                         sudo rm user.bin
                         echo "Introduciendo USER.BIN"
-                        sudo wget http://42561.noip.us/user.bin
-                        #sudo wget https://kg5rki.com/MD380_AIO/user.bin
-                        sudo ./md380-tool spiflashwrite user.bin 0x200000
+                        #sudo wget http://42561.noip.us/user.bin
+                        sudo wget https://kg5rki.com/MD380_AIO/user.bin
+                        sudo ./md380-tool spiflashwrite user.bin 0x1638400
 
                         break;;
                         [nN]* ) echo ""
