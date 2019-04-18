d439015908e43b53fb571309fb9147a871c22d37
ea3eiz <ea3eiz@ea3eiz.com>
2019-04-18 23:30:19 +0200

    Update actualizar_MMDVMHost_05.sh

--- actualizar_MMDVMHost_05.sh
+++ actualizar_MMDVMHost_05.sh
@@ -7369,7 +7369,8 @@ clear
                     PUNTO=";"
                     
                     sed -i "22c $FIJA$HOY1$PUNTO" ~/MMDVMHost/Version.h
-                  
+                    sed -i "145c val \/\= 1.0;" ~/MMDVMHost/Nextion.cpp
+                   
                     #Comprueba si existe el fichero info.ini
                     if [ -f /home/orangepi/Downloads/info.ini ];
                     then
