bf1cf8b5f5a2dc85f6c673c44a99e12ba29efeb9
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-07 17:43:58 +0200

    Q

--- Actualizar_DV4mini_05.sh
+++ Actualizar_DV4mini_05.sh
@@ -1,172 +1,128 @@
 ﻿#!/bin/bash
-clear
 while true
 do
-SCRIPTS_version="SCRIPTS_ORANGE"
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
 clear
 echo "\33[1;32m   *********************************************************************"
 echo "   *                                                                   *"
 echo "   *           Script para Actualizar DV4mini \33[1;33mV.02.05   \33[1;31m by EA3EIZ\33[1;32m     *"
 echo "   *                                                                   *"
 echo "   *********************************************************************"
-echo  "\33[1;36m   1)\33[1;33m Actualizar \33[1;32mDV4MINI20190307   \33[1;33m"
-echo  "\33[1;36m   2)\33[1;33m Volver a la versión \33[1;32mDV4MINI20190127   \33[1;33m"
-echo  "\33[1;36m   3)\33[1;33m Volver a la versión \33[1;32mDV4MINI20170517   \33[1;33m"
-echo  "\33[1;36m   4)\33[1;33m Actualizar \33[1;32mDV4MINI20190307${AMARILLO} (NEW)   \33[1;33m"
+echo  "\33[1;36m   1)\33[1;33m Actualizar DV4mini   \33[1;33m"
+echo  "\33[1;36m   2)\33[1;33m Ir a la versión anterior DV4mini   \33[1;33m"
 echo ""
-echo -n "\33[1;36m   Elige una opción: " 
+echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
+echo ""
+echo -n "\33[1;36m   Elige una opción del 0 al 2: " 
 read escoger_menu
+
 case $escoger_menu in
-1) echo ""
+
+
+
+1) 
+echo ""
 while true
 do
 clear
-                        
-                        ejecutar1=S
-                        case $ejecutar1 in
-                        [sS]* ) echo ""
-                        echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
-                        sudo rm -r ~/$SCRIPTS_version/DV4MINI/
-                        cd ~/$SCRIPTS_version
-                        git clone http://github.com/ea3eiz/DV4MINI
-                        sudo rm -r ~/dv4mini
-                        sudo rm /usr/bin/dv_serial
-                        sudo mkdir ~/dv4mini
-                        cd ~/$SCRIPTS_version/DV4MINI/20190307
+
+	                ejecutar1=S
+		    case $ejecutar1 in
+			[sS]* ) echo ""
+			echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
+                        cd ~/Downloads
+wget http://112.xreflector.es/Soft_DV4MINI/Linux/Raspberry_/dv_serial
+sudo cp -v ~/Downloads/dv_serial ~/dv4mini 
+cd ~/dv4mini/
+sudo chmod 777 dv_serial
+cd ~/Downloads
+sudo rm -v ~/Downloads/dv_serial
+
+sudo cp -v ~/dv4mini/dv4mini.exe ~/dv4mini/dv4mini.exe.old
+wget http://112.xreflector.es/Soft_DV4MINI/Linux/Raspberry_/dv4mini.exe
+sudo cp -v ~/Downloads/dv4mini.exe ~/dv4mini/dv4mini.exe
+sudo chmod -R 777 ~/dv4mini/
+cd ~/Downloads
+sudo rm -v ~/Downloads/dv4mini.exe
+
+sudo chmod -R 777 ~/.config/dv4mini/
+
+sudo rm /usr/bin/dv_serial #este lo copio al que daba error                       
+
+clear
+		break;;
+		[nN]* ) echo ""
+
+			break;;
+esac
+done;;
+
+
+1ultimoquedabaerror) echo ""
+while true
+do
+clear
+
+	                	ejecutar1=S
+		    			case $ejecutar1 in
+						[sS]* ) echo ""
+						echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
+                        cd ~/SCRIPTS_ORANGE
                         cp dv_serial ~/dv4mini
                         cp dv4mini.exe ~/dv4mini
                         cp xref.ip ~/dv4mini
-                        cp dv4k.png ~/dv4mini
                         sudo cp dv_serial /usr/bin/
                         cd /usr/bin/
                         sudo chmod 777 dv_serial
-                        cd ~/dv4mini
-                        sudo chmod 777 dv_serial
-                        sudo cp ~/CRIPTS_version/dv4k.png ~/dv4mini/
+                        sudo chmod -R 777 ~/.config/dv4mini/dv4mini/1.0.0.0
                         clear
-                        echo "************************************"
-                        echo "*** SE HA ACTUALIZADO CON EXITO  ***"
-                        echo "************************************"
-                        sleep 3                        
-                        clear
-                        break;;
-                        [nN]* ) echo ""
-                        break;;
+                        echo "***********************************"
+                        echo "*** SE HA ACTUALIZADO CON EXITO ***"
+                        echo "***********************************"
+                        sleep 3
+                        
+clear
+		break;;
+		[nN]* ) echo ""
+
+			break;;
 esac
 done;;
+
 2) echo ""
 while true
 do
 clear
-                        ejecutar1=S
-                        case $ejecutar1 in
-                        [sS]* ) echo ""
-                        echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
-                        sudo rm -r ~/$SCRIPTS_version/DV4MINI/
-                        cd ~/$SCRIPTS_version
-                        git clone http://github.com/ea3eiz/DV4MINI
-                        sudo rm -r ~/dv4mini
-                        sudo rm /usr/bin/dv_serial
-                        sudo mkdir ~/dv4mini
-                        cd ~/$SCRIPTS_version/DV4MINI/20190127
-                        cp dv_serial ~/dv4mini
-                        cp dv4mini.exe ~/dv4mini
-                        cp xref.ip ~/dv4mini
-                        cp dv4k.png ~/dv4mini
-                        sudo cp dv_serial /usr/bin/
-                        cd /usr/bin/
-                        sudo chmod 777 dv_serial
-                        cd ~/dv4mini
-                        sudo chmod 777 dv_serial
-                        sudo cp ~/CRIPTS_version/dv4k.png ~/dv4mini/
-                        clear
-                        echo "************************************"
-                        echo "*** SE HA ACTUALIZADO CON EXITO  ***"
-                        echo "************************************"
-                        sleep 3                        
-                        clear
-                        break;;
-                        [nN]* ) echo ""
-                        break;;
-esac
-done;;
-3) echo ""
-while true
-do
+
+	                ejecutar1=S
+		    case $ejecutar1 in
+			[sS]* ) echo ""
+			echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
+                        cd ~/Downloads
+wget http://112.xreflector.es/Soft_DV4MINI/Linux/Raspberry_/old/dv_serial
+sudo cp -v ~/Downloads/dv_serial ~/dv4mini 
+cd ~/dv4mini/
+sudo chmod 777 dv_serial
+cd ~/Downloads
+sudo rm -v ~/Downloads/dv_serial 
+
+
+cd ~/Downloads
+sudo cp -v ~/dv4mini/dv4mini.exe ~/dv4mini/dv4mini.exe.old
+wget http://112.xreflector.es/Soft_DV4MINI/Linux/Raspberry_/old/dv4mini.exe
+sudo cp -v ~/Downloads/dv4mini.exe ~/dv4mini/dv4mini.exe
+cd ~/dv4mini/
+sudo chmod 777 dv4mini.exe
+cd ~/Downloads
+sudo rm -v ~/Downloads/dv4mini.exe
+
 clear
-                        ejecutar1=S
-                        case $ejecutar1 in
-                        [sS]* ) echo ""
-                        echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
-                        sudo rm -r ~/$SCRIPTS_version/DV4MINI/
-                        cd ~/$SCRIPTS_version
-                        git clone http://github.com/ea3eiz/DV4MINI
-                        sudo rm -r ~/dv4mini
-                        sudo rm /usr/bin/dv_serial
-                        sudo mkdir ~/dv4mini
-                        cd ~/$SCRIPTS_version/DV4MINI/20170517
-                        cp dv_serial ~/dv4mini
-                        cp dv4mini.exe ~/dv4mini
-                        cp xref.ip ~/dv4mini
-                        cp dv4k.png ~/dv4mini
-                        sudo cp dv_serial /usr/bin/
-                        cd /usr/bin/
-                        sudo chmod 777 dv_serial
-                        cd ~/dv4mini
-                        sudo chmod 777 dv_serial
-                        sudo cp ~/CRIPTS_version/dv4k.png ~/dv4mini/
-                        clear
-                        echo "************************************"
-                        echo "*** SE HA ACTUALIZADO CON EXITO  ***"
-                        echo "************************************"
-                        sleep 3                        
-                        clear
-                        break;;
-                        [nN]* ) echo ""
-                        break;;
+		break;;
+		[nN]* ) echo ""
+
+			break;;
 esac
 done;;
-4) echo ""
-while true
-do
-clear
-                        ejecutar1=S
-                        case $ejecutar1 in
-                        [sS]* ) echo ""
-                        echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
-                        sudo rm -r ~/$SCRIPTS_version/DV4MINI/
-                        cd ~/$SCRIPTS_version
-                        git clone http://github.com/ea3eiz/DV4MINI
-                        sudo rm -r ~/dv4mini
-                        sudo rm /usr/bin/dv_serial
-                        sudo mkdir ~/dv4mini
-                        cd ~/$SCRIPTS_version/DV4MINI/20190307new
-                        cp dv_serial ~/dv4mini
-                        cp dv4mini.exe ~/dv4mini
-                        cp xref.ip ~/dv4mini
-                        cp dv4k.png ~/dv4mini
-                        sudo cp dv_serial /usr/bin/
-                        cd /usr/bin/
-                        sudo chmod 777 dv_serial
-                        cd ~/dv4mini
-                        clear
-                        echo "************************************"
-                        echo "*** SE HA ACTUALIZADO CON EXITO  ***"
-                        echo "************************************"
-                        sleep 3                        
-                        clear
-                        break;;
-                        [nN]* ) echo ""
-                        break;;
-esac
-done;;
+
 0) echo ""
 clear
 echo "\33[1;33m   ******************************"
