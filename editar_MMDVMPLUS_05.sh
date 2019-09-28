897a9bef65d102c67deb9cd99c8d26f523d4ffba
Manuel Díaz Jiménez <manel@iMac-de-Manuel.local>
2019-09-28 20:01:17 +0200

    Update editar_MMDVMPLUS_05.sh

--- editar_MMDVMPLUS_05.sh
+++ editar_MMDVMPLUS_05.sh
@@ -67,7 +67,7 @@ url=`grep -n "URL" ~/MMDVMHost/MMDVMPLUS.ini`
 url1=`expr substr $url 4 30`
 echo "$url1"
 
-echo "\33[1;36m   6)\33[0m Utilizar puerto interno (ttyS0)\33[1;33m"
+echo "\33[1;36m   6)\33[0m Utilizar puerto interno (ttyS3)\33[1;33m"
 echo "\33[1;36m   7)\33[0m Utilizar puerto USB (ttyACM0)\33[1;33m"
 echo "\33[1;36m   8)\33[0m Utilizar puerto USB (ttyACM1)\33[1;33m"
 echo "\33[1;36m   9)\33[0m Utilizar puerto USB (ttyUSB0)\33[1;33m"
@@ -513,7 +513,7 @@ do
                           [sS]* ) echo ""
                           letrac=c
                           numero_linea_port=$numero_linea_port$letrac
-                          sed -i "$numero_linea_port Port=/dev/ttyS0" ~/MMDVMHost/MMDVMPLUS.ini
+                          sed -i "$numero_linea_port Port=/dev/ttyS3" ~/MMDVMHost/MMDVMPLUS.ini
         break;;
         [nN]* ) echo ""
         break;;
