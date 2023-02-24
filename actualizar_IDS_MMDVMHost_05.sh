542026bb2af75b11e1705deca87ece051326d295
Manel <ea3eiz@ea3eiz.com>
2023-02-24 12:11:04 +0100

    1

--- actualizar_IDS_MMDVMHost_05.sh
+++ actualizar_IDS_MMDVMHost_05.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/bash
 clear
-echo "\33[1;32m ACTUALIZANDO IDS INDICATIVOS ea4aoj >>>>>"
+echo "\33[1;32m ACTUALIZANDO IDS INDICATIVOS ea3eiz >>>>>"
 sleep 10
 var=`grep -n -m 1 '\<DMRIDPATH\>' /home/orangepi/MMDVMHost/linux/DMRIDUpdate.sh`
 buscar=":"
