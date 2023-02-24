04d9bed0e54a9bf3a794bae212fe71b950a1ce91
Manel <ea3eiz@ea3eiz.com>
2023-02-24 10:57:11 +0100

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sed -i "6c Exec=sudo sh cerrar_solodstar.sh" ~/Desktop/AbrirsoloDstar.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" ~/Desktop/AbrirsoloDstar.desktop
-sed -i "11c Name[es_ES]=Cerrar solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
+sed -i "6c Exec=sudo sh cerrar_solodstar.sh" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR_ON.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+sed -i "11c Name[es_ES]=Cerrar solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 
-sed -i "13c SOLODSTAR=ON" ~/status.ini
+sed -i "13c SOLODSTAR=ON" /home/orangepi/status.ini
 
-cd ~/MMDVMHost
+cd /home/orangepi/MMDVMHost
 echo "\33[1;32m"
 sudo ./MMDVMDSTAR MMDVMDSTAR.ini & ircddbgateway -gui
 
-sed -i "6c Exec=mate-terminal -x sh -c 'cd /home/orangepi/SCRIPTS_ORANGE/;sh ejecutar_solodstar.sh'" ~/Desktop/AbrirsoloDstar.desktop
-sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR.png" ~/Desktop/AbrirsoloDstar.desktop
-sed -i "11c Name[es_ES]=Abrir solo D-STAR" ~/Desktop/AbrirsoloDstar.desktop
-sed -i "13c SOLODSTAR=OFF" ~/status.ini
+sed -i "6c Exec=mate-terminal -x sh -c 'cd /home/orangepi/SCRIPTS_ORANGE/;sh ejecutar_solodstar.sh'" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+sed -i "13c SOLODSTAR=OFF" /home/orangepi/status.ini
