f6540c43a9c4545887027abd1a621fd7655a76c7
Manel <ea3eiz@ea3eiz.com>
2023-03-06 20:21:08 +0100

    1

--- cerrar_dump1090.sh
+++ cerrar_dump1090.sh
@@ -5,10 +5,9 @@ killall dump1090
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_BlueDV.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dump1090.sh'" /home/pi/Abrir_BlueDV.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AVION_OFF.png" /home/pi/Abrir_BlueDV.desktop
-sed -i "10c Name[es_ES]=Abrir Dump1090" /home/pi/Abrir_BlueDV.desktop
-#sed -i "7c MMDVMBM=ON" /home/pi/status.ini
+sed -i "6c Exec=sh ejecutar_dump1090" /home/orangepi/Desktop/BlueDV.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/ICONO_AVION_OFF.png" /home/orangepi/Desktop/BlueDV.desktop
+sed -i "10c Name[es_ES]=Abrir Dumo1090" /home/orangepi/Desktop/BlueDV.desktop
 cd /home/pi
 sudo cp Abrir_BlueDV.desktop /home/pi/Desktop
 #
