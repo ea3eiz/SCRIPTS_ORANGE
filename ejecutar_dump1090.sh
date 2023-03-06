c794b0973b230204b01d274b9bbe8c99c6ef5ec8
Manel <ea3eiz@ea3eiz.com>
2023-03-06 22:09:39 +0100

    1

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -19,12 +19,3 @@ else
 xterm -geometry 88x51+22+0 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e /home/orangepi/dump1090/dump1090 --net --interactive --net-ro-port $raw --net-bo-port $beast --ppm $ppm --net-http-port $http
 fi
 
-#cd /home/orangepi/Desktop
-#sudo cp BlueDV.desktop /home/pi
-#sed -i "4c Exec=sh -c 'cd /home/orangepi/$SCRIPTS_version; sh ejecutar_dump1090.sh'" /home/orangepi/BlueDV.desktop
-#sed -i "5c Icon=/home/orangepi/$SCRIPTS_version/ICONO_AVION_OFF.png" /home/orangepi/BlueDV.desktop
-#sed -i "10c Name[es_ES]=Abrir Dump1090" /home/orangepi/BlueDV.desktop##sed -i "7c MMDVMBM=OFF" /home/orangepi/status.ini
-#cd /home/pi
-#sudo cp BlueDV.desktop /home/orangepi/Desktop
-#sleep 1
-#sudo rm /home/orangepi/BlueDV.desktop
