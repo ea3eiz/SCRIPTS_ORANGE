a0449e6e6ba1525585816d2aee9fa73236d11fc3
Manel <ea3eiz@ea3eiz.com>
2023-03-06 19:25:26 +0100

    11

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -21,38 +21,19 @@ beast=$(awk "NR==50" /home/orangepi/status.ini)
 if [ "$stick" = 'RSP1' ];then
 xterm -geometry 88x51+22+0 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo /home/orangepi/dump1090_sdrplay/dump1090 --net --interactive --gain $gain --dev-sdrplay
 
-
-#xterm -geometry 88x51+22+0 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo /home/orangepi/dump1090_sdrplay/dump1090 --net --interactive --dev-sdrplay --gain -10
-
-#/home/orangepi/dump1090_sdrplay/dump1090 --net --interactive --gain -10 --dev-sdrplay
-
-
 elif [ "$gain" = '-10' ];then
 xterm -geometry 88x51+22+0 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e /home/orangepi/dump1090/dump1090 --net --interactive --net-ro-port $raw --net-bo-port $beast --gain $gain --ppm $ppm --net-http-port $http
 else
 xterm -geometry 88x51+22+0 -bg black -fg green -fa ‘verdana’ -fs 9x -T DUMP1090 -e /home/orangepi/dump1090/dump1090 --net --interactive --net-ro-port $raw --net-bo-port $beast --ppm $ppm --net-http-port $http
 fi
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-cd /home/orangepi/Desktop
-sudo cp BlueDV.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/orangepi/$SCRIPTS_version; sh ejecutar_dump1090.sh'" /home/orangepi/BlueDV.desktop
-sed -i "5c Icon=/home/orangepi/$SCRIPTS_version/ICONO_AVION_OFF.png" /home/orangepi/BlueDV.desktop
-sed -i "10c Name[es_ES]=Abrir Dump1090" /home/orangepi/BlueDV.desktop
-#sed -i "7c MMDVMBM=OFF" /home/orangepi/status.ini
-cd /home/pi
-sudo cp BlueDV.desktop /home/orangepi/Desktop
-sleep 1
-sudo rm /home/orangepi/BlueDV.desktop
+#cd /home/orangepi/Desktop
+#sudo cp BlueDV.desktop /home/pi
+#sed -i "4c Exec=sh -c 'cd /home/orangepi/$SCRIPTS_version; sh ejecutar_dump1090.sh'" /home/orangepi/BlueDV.desktop
+#sed -i "5c Icon=/home/orangepi/$SCRIPTS_version/ICONO_AVION_OFF.png" /home/orangepi/BlueDV.desktop
+#sed -i "10c Name[es_ES]=Abrir Dump1090" /home/orangepi/BlueDV.desktop
+##sed -i "7c MMDVMBM=OFF" /home/orangepi/status.ini
+#cd /home/pi
+#sudo cp BlueDV.desktop /home/orangepi/Desktop
+#sleep 1
+#sudo rm /home/orangepi/BlueDV.desktop
