a04396df61e9490f46fe509b4b1aadc184fc73ae
Manel <ea3eiz@ea3eiz.com>
2023-03-06 20:18:14 +0100

    1

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd /home/orangepi/Desktop
-sudo sed -i "6c Exec=sh -c 'cd /home/orangepi/SCRIPTS_ORANGE;sh cerrar_dump1090.sh'"
-sudo sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/ICONO_AVION_ON.png"
-sudo sed -i "10c Name[es_ES]=Cerrar Dump1090"  
+sed -i "6c Exec=sh cerrar_dump1090" /home/orangepi/Desktop/BlueDV.desktop
+sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/ICONO_AVION_ON.png" /home/orangepi/Desktop/BlueDV.desktop
+sed -i "10c Name[es_ES]=Cerrar Dumo1090" /home/orangepi/Desktop/BlueDV.desktop
 
 stick=$(awk "NR==40" /home/orangepi/status.ini)
 raw=$(awk "NR==42" /home/orangepi/status.ini)
