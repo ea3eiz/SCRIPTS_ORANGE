dd16bf97816280f83008139220a8ed9acf30fc88
Manel <ea3eiz@ea3eiz.com>
2023-03-06 20:54:50 +0100

    1

--- cerrar_dump1090.sh
+++ cerrar_dump1090.sh
@@ -4,4 +4,4 @@ sudo killall dump1090
 
 sed -i "6c Exec=sh -c 'cd /home/orangepi/SCRIPTS_ORANGE;sh ejecutar_dump1090.sh' /home/orangepi/Desktop/BlueDV.desktop"
 sed -i "7c Icon=/home/orangepi/SCRIPTS_ORANGE/ICONO_AVION_ON.png" /home/orangepi/Desktop/BlueDV.desktop
-sed -i "10c Name[es_ES]=Abrie Dump1090" /home/orangepi/Desktop/BlueDV.desktop
+sed -i "10c Name[es_ES]=Abrir Dump1090" /home/orangepi/Desktop/BlueDV.desktop
