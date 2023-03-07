de5b96e09ca7715b34caca2434b43db2bfb5ece6
Manel <ea3eiz@ea3eiz.com>
2023-03-07 13:14:43 +0100

    1

---
+++ actualiza_imagen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash	                
+                        git pull 
+                        sleep 3
+                        cd /home/orangepi/
+                        sudo chmod 777 SCRIPTS_ORANGE
+                        clear
+                        cd /home/orangepi/AUTOSTART
+                        git pull
+
+                        sudo usermod -a -G dialout orangepi
+                        sudo usermod -a -G uucp orangepi
+
+                        sudo rm /home/orangepi/qt
+
+                        cd /home/orangepi/SCRIPTS_ORANGE
+                        cp qt* /home/orangepi/qt
+
+                        chmod 777 -R /home/orangepi/qt
